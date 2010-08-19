a5263ce6570fe1e8a78d9f9f98e13f82ff0d09e2
pixie16 <pixie16@pc0084117.phy.ornl.gov>
2010-08-19 16:50:13 -0400

    Added Interface commands to swap bits in the CSRA Fixed deprecated conversion from const char * to char * in Interface Interface now defines PIF_REVx to control which version of the Pixie cards it is compiled for.

--- Interface/PixieInterface.h
+++ Interface/PixieInterface.h
@@ -14,16 +14,30 @@
 #define PIXIE16_REVD_GENERAL 999 // arbitrary large number
 #endif
 
+#if PIXIE16_REVISION == PIXIE16_REVD_GENERAL
+#define PIF_REVD
+#else
+#define PIF_REVA
+#endif
+
 #include <string>
 
 #include <stdint.h>
 
 #include "Lock.h"
 
+#ifdef PIF_REVD
+const int CCSRA_PILEUP  = 15;
+const int CCSRA_CATCHER = 16;
+#endif
+
 class PixieInterface
 {
  public:
   static const size_t STAT_SIZE = N_DSP_PAR - DSP_IO_BORDER;
+#ifdef PIF_REVD
+  enum CatcherModes {PC_STANDARD, PC_REJECT, PC_HYBRID, PC_ACCEPT};
+#endif
 
   typedef uint32_t word_t;
   typedef uint16_t halfword_t;
@@ -38,13 +52,13 @@ class PixieInterface
   // wrappers to the pixie-16 app functions
   bool Init(bool offlineMode = false);
   bool Boot(int mode = 0x7f, bool useWorkingSetFile = false);
-  bool WriteSglModPar(char *name, word_t val, int mod);
-  bool ReadSglModPar(char *name, word_t *pval, int mod);
-  void PrintSglModPar(char *name, int mod);
-  bool WriteSglChanPar(char *name, double val, int mod, int chan);
-  bool ReadSglChanPar(char *name, double *pval, int mod, int chan);
-  void PrintSglChanPar(char *name, int mod, int chan);
-  bool SaveDSPParameters(char *fn = NULL);
+  bool WriteSglModPar(const char *name, word_t val, int mod);
+  bool ReadSglModPar(const char *name, word_t *pval, int mod);
+  void PrintSglModPar(const char *name, int mod);
+  bool WriteSglChanPar(const char *name, double val, int mod, int chan);
+  bool ReadSglChanPar(const char *name, double *pval, int mod, int chan);
+  void PrintSglChanPar(const char *name, int mod, int chan);
+  bool SaveDSPParameters(const char *fn = NULL);
   bool AcquireTraces(int mod);
   // # AcquireTraces must be called before calling this #
   bool ReadSglChanTrace(unsigned short *buf, unsigned long sz,
@@ -68,7 +82,7 @@ class PixieInterface
   bool CheckRunStatus(void); // check status in all modules
   bool CheckRunStatus(int mod);
 
-#if PIXIE16_REVISION == PIXIE16_REVD_GENERAL
+#ifdef PIF_REVD
   unsigned long CheckFIFOWords(unsigned short mod);
   bool ReadFIFOWords(word_t *buf, unsigned long nWords,
 		     unsigned short mod);
@@ -92,10 +106,30 @@ class PixieInterface
     BootDSP = 0x08, DownloadParameters = 0x10, ProgramFPGA = 0x20,
     SetDAC = 0x40, BootAll = 0x7f
   };
+
+  /** Handy functions for manipulating CSRA/B */
+  bool SwapGain(int mod, int chan);
+  bool SwapGood(int mod, int chan);
+  bool SwapPolarity(int mod, int chan);
+
+#ifdef PIF_REVD
+  bool SwapCatcherBit(int mod, int chan);
+  bool SwapPileupBit(int mod, int chan);
+  bool SwapTraceCapture(int mod, int chan);
+
+  bool SetProtonCatcherMode(int mod, int chan, CatcherModes mode);
+#endif
+
  private:
+  bool SwapChannelBit(int mod, int chan, const char *parameter, int bit);
+
   static const size_t MAX_MODULES = 14;
   static const size_t CONFIG_LINE_LENGTH = 80;
   static const size_t TRACE_LENGTH = RANDOMINDICES_LENGTH;
+
+#ifdef PIF_REVD
+  void CatcherMessage(void);
+#endif
 
   std::string pixieBaseDir;
   std::string spFpgaFile;
@@ -125,6 +159,11 @@ class PixieInterface
 
   word_t extraWord[MAX_MODULES];
   bool hasExtra[MAX_MODULES];
+
+  // temporary variables which hold the parameter which is being modified
+  //   to deal with the const-incorrectness of the Pixie-16 API
+  static const size_t nameSize=120;
+  char tmpName[nameSize];
 };
 
 #endif // __PIXIEINTERFACE_H_
