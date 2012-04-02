8352cb5203df0e4df3e55b599faf1304385dd828
David Miller <damiller@mailaps.org>
2012-04-02 08:58:43 -0400

    Updates to the analysis bash scripts

--- online.bash
+++ online.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo -e "zero\nshm\ngo\nend\n" | ./pixie_ldf_c_online HIS/current
+if [[ -x ./pixie_ldf_c_online ]]; then
+    PROG=./pixie_ldf_c_online
+else
+    PROG=./pixie_ldf_c
+fi
+
+if [[ ! -d ${HISDIR:=HIS} ]]; then
+    echo "Histogram directory $HISDIR does not exist."
+    exit 1
+fi
+
+echo -e "zero\nshm\ngo\nend\n" | $PROG $HISDIR/current
