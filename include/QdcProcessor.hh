7b9fe510558eacb348e1e5078fd4698e7257a618
D T Miller <damiller@mailaps.org>
2012-04-02 10:38:15 +0200

    Updating QDC processor for position determination

--- include/QdcProcessor.hh
+++ include/QdcProcessor.hh
@@ -5,11 +5,33 @@
 #ifndef __QDCPROCESSOR_HH_
 #define __QDCPROCESSOR_HH_
 
+#include <string>
+#include <vector>
+
 #include "EventProcessor.h"
+
+class ChanEvent;
+
 class QdcProcessor : public EventProcessor
 {
+private:
+    static const std::string configFile;
+    static const int numLocations = 16;
+    static const int numQdcs = 8;
+	
+    float qdcLen[numQdcs]; //< the length of each qdc in pixie samples
+    float totLen;          //< calculated length of all qdcs excluding baseline qdc
+    int whichQdc;          //< which qdc we are using for position determination
+    float posScale;        //< an arbitrary scale for the position parameter to physical units
+    float minNormQdc[numLocations]; //< the minimum normalized qdc observed for a location
+    float maxNormQdc[numLocations]; //< the maximum normalized qdc observed for a location
+
+    ChanEvent* FindMatchingEdge(ChanEvent *match,
+				std::vector<ChanEvent*>::const_iterator begin, 
+				std::vector<ChanEvent*>::const_iterator end) const;				
 public:
     QdcProcessor(); // no virtual c'tors
+    virtual bool Init(DetectorDriver &driver);
     virtual bool Process(RawEvent &event);
     virtual void DeclarePlots(void) const;
 };
--- src/QdcProcessor.cpp
+++ src/QdcProcessor.cpp
@@ -3,7 +3,8 @@
  * Handle some QDC action
  */
 
-#include <iomanip>
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <numeric>
 #include <sstream>
@@ -14,151 +15,270 @@
 
 using namespace std;
 
+namespace dammIds {
+    namespace qdc {
+	const int QDC_JUMP = 20;
+	const int LOC_SUM  = 18;
+
+	const int D_QDCNORMN_LOCX        = 2300;
+	const int D_QDCTOTNORM_LOCX      = 2460;
+	const int D_INFO_LOCX            = 2480;
+	const int DD_QDCN__QDCN_LOCX     = 2500;
+	const int DD_QDCTOT__QDCTOT_LOCX = 2660;
+	const int DD_POSITION            = 2699;
+    }
+}
+
+const string QdcProcessor::configFile("qdc.txt");
+
+/**
+ * Initialize the qdc to handle ssd events
+ */
 QdcProcessor::QdcProcessor() : EventProcessor()
 {
     name="qdc";
     associatedTypes.insert("ssd");
 }
 
+/**
+ * Reads in QDC parameters from an input file
+ *   The file format allows comment lines at the beginning
+ *   Followed by QDC lengths 
+ *   Which QDC to use for position calculation
+ *     followed by the amount to scale the [0,1] result by to physical units
+ *   And min and max values of the normalized QDC for each location in form:
+ *      <location> <min> <max>
+ *   Note that QDC 0 is considered to be a baseline section of the trace for
+ *     baseline removal for the other QDCs
+ */
+bool QdcProcessor::Init(DetectorDriver &driver)
+{
+    // Call the parent function to handle the standard stuff
+    if (!EventProcessor::Init(driver)) {
+	return false;
+    }
+
+    ifstream in(configFile.c_str());
+    if (!in) {
+	cerr << "Failed to open the QDC parameter file, QDC processor disabled." << endl;
+	return (initDone = false);	
+    }
+
+    // Ignore any lines at the beginning that don't have a digit
+    // This allows for some comments at the beginning of the file
+    int linesIgnored = 0;
+    while ( !isdigit(in.peek()) ) {
+	in.ignore(1000, '\n');
+	linesIgnored++;
+    }
+    if (linesIgnored != 0) {
+	cout << "Ignored " << linesIgnored << " comment lines in " 
+	     << configFile << endl;
+    }
+
+    for (int i=0; i < numQdcs; i++) 
+	in >> qdcLen[i];
+    totLen = accumulate(qdcLen + 1, qdcLen + 8, 0);
+
+    in >> whichQdc >> posScale;
+
+    int numLocationsRead = 0;
+    while (!in.eof()) {
+	int location;
+	in >> location;
+	in >> minNormQdc[location-1] >> maxNormQdc[location-1];
+	numLocationsRead++;
+    }
+    if (numLocationsRead != numLocations) {
+	cerr << "Only read QDC position calibration information from "
+	     << numLocationsRead << " locations where a total of "
+	     << numLocations << " was expected!" << endl;
+	cerr << "  Disabling QDC processor." << endl;
+	return (initDone = false);
+    }
+    
+    cout << "QDC processor initialized with " << numLocations 
+	 << " locations operating on " << numQdcs << " QDCs" << endl;
+    cout << "  QDC #" << whichQdc << " being used for position determination."
+	 << endl;
+
+    return true;
+}
+
+/**
+ *  Declare all the plots we plan on using (part of dammIds::qdc namespace)
+ */
 void QdcProcessor::DeclarePlots(void) const
 {
-    const int qdcBins = S8;
+    using namespace dammIds::qdc;
 
-    for (int i=0; i < 16; i++) {	
+    const int qdcBins = S8;
+    const int normBins = SA;
+    const int infoBins = S3;
+
+    for (int i=0; i < numLocations; i++) {	
 	stringstream str;
 	for (int j=1; j < 8; j++) {
 	    str << "QDC " << j << ", T/B LOC " << i;
-	    DeclareHistogram2D(2500 + 20 * j + i , qdcBins, qdcBins, str.str().c_str() );
+	    DeclareHistogram2D(DD_QDCN__QDCN_LOCX + QDC_JUMP * j + i , qdcBins, qdcBins, str.str().c_str() );
 	    str.str("");
+	    str << "QDC " << j << " NORM T/B LOC" << i;
+	    DeclareHistogram1D(D_QDCNORMN_LOCX + QDC_JUMP * j + i, normBins, str.str().c_str() );
+	    str.str("");
+
 	    if (i == 0) {
-		// declare once
+		// declare only once
 		str << "ALL QDC T/B" << j;
-		DeclareHistogram2D(2516 + 20 * j, qdcBins, qdcBins, str.str().c_str() );
+		DeclareHistogram2D(DD_QDCN__QDCN_LOCX + QDC_JUMP * j + LOC_SUM, 
+				   qdcBins, qdcBins, str.str().c_str() );
 		str.str("");
-		str << "ALL QDC NORM T/B" << j;
-		DeclareHistogram1D(2517 + 20 * j, SA, str.str().c_str() );
-		str.str("");
+		str << "ALL QDC " << j << " NORM T/B";   
+		DeclareHistogram1D(D_QDCNORMN_LOCX + QDC_JUMP * j + LOC_SUM, 
+				   normBins, str.str().c_str() );
 	    }
 	}
 	str << "QDCTOT T/B LOC " << i;
-	DeclareHistogram2D(2660 + i, qdcBins, qdcBins, str.str().c_str() );
+	DeclareHistogram2D(DD_QDCTOT__QDCTOT_LOCX + i, qdcBins, qdcBins, str.str().c_str() );
 	str.str("");
+	str << "QDCTOT NORM T/B LOC " << i;
+	DeclareHistogram1D(D_QDCTOTNORM_LOCX + i, normBins, str.str().c_str());
+
+	str << "INFO LOC " << i;
+	DeclareHistogram1D(D_INFO_LOCX + i, infoBins, str.str().c_str());
     }
-    DeclareHistogram2D(2676, qdcBins, qdcBins, "ALL QDCTOT T/B");
-    DeclareHistogram1D(2677, SA, "ALL QDCTOT NORM T/B");
+    DeclareHistogram2D(DD_QDCTOT__QDCTOT_LOCX + LOC_SUM, qdcBins, qdcBins, "ALL QDCTOT T/B");
+    DeclareHistogram1D(D_QDCTOTNORM_LOCX + LOC_SUM, normBins, "ALL QDCTOT NORM T/B");
+
+    DeclareHistogram1D(D_INFO_LOCX + LOC_SUM, infoBins, "ALL INFO");
+    DeclareHistogram2D(DD_POSITION, S4, S6, "Qdc Position");
 }
 
+/**
+ *  Process the QDC data involved in top/bottom side for a strip 
+ *  Note QDC lengths are HARD-CODED at the moment for the plots and to determine the position
+ */
 bool QdcProcessor::Process(RawEvent &event)
 {
     if (!EventProcessor::Process(event))
 	return false;
 
-    static const vector<ChanEvent*> &implantEvents = 
-	event.GetSummary("ssd:implant", true)->GetList();
+    static const vector<ChanEvent*> &sumEvents = 
+	event.GetSummary("ssd:sum", true)->GetList();
+    static const vector<ChanEvent*> &digisumEvents =
+	event.GetSummary("ssd:digisum", true)->GetList();
     static const vector<ChanEvent*> &topEvents =
 	event.GetSummary("ssd:top", true)->GetList();
     static const vector<ChanEvent*> &bottomEvents =
-	event.GetSummary("ssd:bottm", true)->GetList();
+	event.GetSummary("ssd:bottom", true)->GetList();
 
-    for (vector<ChanEvent*>::const_iterator it=implantEvents.begin();
-	 it != implantEvents.end(); it++) {
-	ChanEvent *chan = *it;
-	
-	const Identifier& id = chan->GetChanID();
-	int location = id.GetLocation();
-	double time = chan->GetTime();
+    vector<ChanEvent *> allEvents;
+    // just add in the digisum events for now
+    allEvents.insert(allEvents.begin(), digisumEvents.begin(), digisumEvents.end());
+    allEvents.insert(allEvents.begin(), sumEvents.begin(), sumEvents.end());
 
-	if (chan->GetQdcValue(0) == U_DELIMITER)
-	    continue;
-	/*
-	// find the matching events for the edges
-	for (vector<ChanEvent*>::const_iterator itEdge = edgeEvents.begin();
-	     itEdge != edgeEvents.end(); itEdge++) {
-	    ChanEvent *edge = *itEdge;
+    for (vector<ChanEvent*>::const_iterator it=allEvents.begin();
+	 it != allEvents.end(); it++) {
+	ChanEvent *sumchan   = *it;
 
-	    const Identifier& edgeid = edge->GetChanID();
-	    double edgeTime = edge->GetTime();
-	    int edgeLocation = edgeid.GetLocation();
+	const ChanEvent *top    = FindMatchingEdge(sumchan, topEvents.begin(), topEvents.end());
+	const ChanEvent *bottom = FindMatchingEdge(sumchan, bottomEvents.begin(), bottomEvents.end());
 
-	    if ( (edgeLocation % 8) != location)
-		continue;
-	    if (abs(edgeTime - time) > 100)
-		continue;
+	int location = sumchan->GetChanID().GetLocation();
+	if (top == NULL || bottom == NULL) {
+	    using namespace dammIds::qdc;
 
-	    if (edgeLocation == location)
-		cout << "      RGHT QDC: ";
-	    if (edgeLocation == location + 8)
-		cout << "      LEFT QDC: ";
-	    for (int i=0; i < 8; i++) {
-		cout << setw(6) << edge->GetQdcValue(i);
+	    if (top == NULL) {
+		plot(D_INFO_LOCX + location - 1, 3);
+		plot(D_INFO_LOCX + LOC_SUM, 3);
 	    }
-	    cout << endl;
+
+	    if (bottom == NULL) {
+		plot(D_INFO_LOCX + location - 1, 4);
+		plot(D_INFO_LOCX + LOC_SUM, 4);
+	    }
+	    continue;
+	}
+
+	/* Make sure we get the same match going backwards to insure there is only one in the vector */
+	/* Isn't working due to some C++ STL mumbo jumbo -- normal iterators and reverse iterators not interchangeable 
+	if ( FindMatchingEdge(sumchan, topEvents.rbegin(), topEvents.rend()) != top) {
+	    cout << "Multiple top edges found for sum location " << location << endl; 
+	}
+	if ( FindMatchingEdge(sumchan, bottomEvents.rbegin(), bottomEvents.rend()) != bottom) {
+	    cout << "Multiple top edges found for sum location " << location << endl; 
 	}
 	*/
-    }
-
-    float qdcLen[8] = {40., 30., 30., 50., 50., 100., 600., 50.};
-    float totLen = accumulate(qdcLen + 1, qdcLen + 8, 0);
-
-    // for each location
-    for (int loc=1; loc <= 16; loc++) {
-	float topQdc[8] = {-1};
-	float bottomQdc[8] = {-1};
-	float topQdcTot = -1;
-	float bottomQdcTot = -1;
-
-	for (vector<ChanEvent*>::const_iterator itTop = topEvents.begin();
-	     itTop != topEvents.end(); itTop++) {
-	    ChanEvent *edge = *itTop;
+	using namespace dammIds::qdc;
 	
-	    const Identifier& edgeid = edge->GetChanID();
-	    double edgeTime = edge->GetTime();
-	    if (edgeid.GetLocation() != loc) 
-		continue;	    
-	    if (edge->GetQdcValue(0) == U_DELIMITER)
-		continue;
-
-	    topQdcTot = 0;
-	    for (int i=1; i < 8; i++) {		
-		topQdc[i] = edge->GetQdcValue(i);
-		topQdc[i] -= edge->GetQdcValue(0) * qdcLen[i] / qdcLen[0];
-		topQdcTot += topQdc[i];
-		topQdc[i] /= qdcLen[i];		
-	    }
-	    topQdcTot /= totLen;
-	}
-	for (vector<ChanEvent*>::const_iterator itBottom = bottomEvents.begin();
-	     itBottom != bottomEvents.end(); itBottom++) {
-	    ChanEvent *edge = *itBottom;
+	float topQdc[numQdcs];
+	float bottomQdc[numQdcs];
+	float topQdcTot = 0;
+	float bottomQdcTot = 0;
+	float position = NAN;
 	
-	    const Identifier& edgeid = edge->GetChanID();
-	    double edgeTime = edge->GetTime();
-	    if (edgeid.GetLocation() != loc) 
-		continue;	    
-	    if (edge->GetQdcValue(0) == U_DELIMITER)
-		continue;
-	    bottomQdcTot = 0;
-	    for (int i=1; i < 8; i++) {		
-		bottomQdc[i] = edge->GetQdcValue(i);
-		bottomQdc[i] -= edge->GetQdcValue(0) * qdcLen[i] / qdcLen[0];
-		bottomQdcTot += bottomQdc[i];
-		bottomQdc[i] /= qdcLen[i];
+	if (bottom->GetQdcValue(0) == U_DELIMITER || top->GetQdcValue(0) == U_DELIMITER) {
+#ifdef VERBOSE
+	    cout << "SSD strip edges are missing QDC information for location " << location << endl;
+#endif
+	    if (top->GetQdcValue(0) == U_DELIMITER) {
+		plot(D_INFO_LOCX + location - 1, 1);
+		plot(D_INFO_LOCX + LOC_SUM, 1);
 	    }
-	    bottomQdcTot /= totLen;
-	}
-	if (topQdcTot != -1 && bottomQdcTot != -1) {
-	    for (int j=1; j < 8; j++) {
-		plot(2500 + 20 * j + loc - 1, topQdc[j], bottomQdc[j]);
-		plot(2500 + 20 * j + 16, topQdc[j], bottomQdc[j]);
-		plot(2500 + 20 * j + 17, topQdc[j] * 1000. / (topQdc[j] + bottomQdc[j])); // per mil
+	    if (bottom->GetQdcValue(0) == U_DELIMITER) {
+		plot(D_INFO_LOCX + location - 1, 2);
+		plot(D_INFO_LOCX + LOC_SUM, 2);		
 	    }
-	    plot(2660 + loc - 1, topQdcTot, bottomQdcTot);
-	    plot(2676, topQdcTot, bottomQdcTot);
-	    plot(2677, topQdcTot * 1000. / (topQdcTot + bottomQdcTot));
+	    continue;
 	}
-    }
+	plot(D_INFO_LOCX + location - 1, 0); // good stuff
+	plot(D_INFO_LOCX + LOC_SUM     , 0); // good stuff
+
+	for (int i=1; i < numQdcs; i++) {		
+	    topQdc[i] = top->GetQdcValue(i);
+	    topQdc[i] -= top->GetQdcValue(0) * qdcLen[i] / qdcLen[0];
+	    topQdcTot += topQdc[i];
+	    topQdc[i] /= qdcLen[i];		
+	    
+	    bottomQdc[i] = bottom->GetQdcValue(i);
+	    bottomQdc[i] -= bottom->GetQdcValue(0) * qdcLen[i] / qdcLen[0];
+	    bottomQdcTot += bottomQdc[i];
+	    bottomQdc[i] /= qdcLen[i];
+	    
+	    plot(DD_QDCN__QDCN_LOCX + QDC_JUMP * i + location - 1, topQdc[i], bottomQdc[i]);
+	    plot(DD_QDCN__QDCN_LOCX + QDC_JUMP * i + LOC_SUM, topQdc[i], bottomQdc[i]);
+	    float frac = topQdc[i] / (topQdc[i] + bottomQdc[i]) * 1000.; // per mil
+	    plot(D_QDCNORMN_LOCX + QDC_JUMP * i + location - 1, frac);
+	    plot(D_QDCNORMN_LOCX + QDC_JUMP * i + LOC_SUM, frac);
+	    if (i == whichQdc) {
+		position = posScale * (frac - minNormQdc[location]) / 
+		    (maxNormQdc[location] - minNormQdc[location]);
+		sumchan->GetTrace().InsertValue("position", position);
+		plot(DD_POSITION, location, position);
+	    }
+	} // end loop over qdcs
+	topQdcTot    /= totLen;
+	bottomQdcTot /= totLen;
+	
+	plot(DD_QDCTOT__QDCTOT_LOCX + location - 1, topQdcTot, bottomQdcTot);
+	plot(DD_QDCTOT__QDCTOT_LOCX + LOC_SUM, topQdcTot, bottomQdcTot);
+    } // end iteration over sum events
 
     EndProcess();
     return true;
+}
+
+ChanEvent* QdcProcessor::FindMatchingEdge(ChanEvent *match,
+					  vector<ChanEvent*>::const_iterator begin,
+					  vector<ChanEvent*>::const_iterator end) const
+{
+    const float timeCut = 100.; // maximum difference between edge and sum timestamps
+	
+    for (;begin < end; begin++) {
+	if ( (*begin)->GetChanID().GetLocation() == match->GetChanID().GetLocation() &&
+	     abs( (*begin)->GetTime() - match->GetTime() ) < timeCut ) {
+	    return *begin;
+	}
+    }
+    return NULL;
 }
 
