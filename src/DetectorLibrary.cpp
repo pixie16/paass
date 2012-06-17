ac0a770ad07bc40f64408bc55811f547ed81b52a
S.V. Paulauskas <stanpaulauskas@gmail.com>
2012-06-17 16:23:24 -0400

    Still do not have things fully integrated. Closer...

--- src/DetectorLibrary.cpp
+++ src/DetectorLibrary.cpp
@@ -208,11 +208,11 @@ void DetectorLibrary::PrintUsedDetectors(void) const
 */
 const set<string>& DetectorLibrary::GetKnownDetectors(void)
 {
-    const unsigned int detTypes = 17;
+    const unsigned int detTypes = 19;
     const string detectorStrings[detTypes] = {
 	"dssd_front", "dssd_back", "idssd_front", "position", "timeclass",
-	"ge", "si", "scint", "mcp", "mtc", "generic", "ssd", "vandle",
-	"pulser", "logic", "ion_chamber", "ignore"};
+	"ge", "si", "scint", "mcp", "mtc", "generic", "ssd", "vandleSmall",
+	"vandleBig", "tvandle","pulser", "logic", "ion_chamber", "ignore"};
   
     // only call this once
     if (!knownDetectors.empty())
