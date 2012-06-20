ea5f0042f374445684a58392a382441986dbd2a5
David Miller <damiller@mailaps.org>
2012-06-20 13:11:31 -0400

    Added a file to get arguments from the command line.

---
+++ include/GetArguments.hpp
@@ -0,0 +1,39 @@
+/** \file GetArguments.hpp
+ *
+ * A quick and dirty way to get the arguments pass to scan through
+ *   the respective Fortran calls
+ *
+ *   David Miller, June 2012
+ */
+
+#ifndef __GETARGUMENTS_HPP_
+#define __GETARGUMENTS_HPP_
+
+#ifdef LINK_GFORTRAN
+#define GETARG__GETARGS _gfortran_getarg_i4
+#define IARGC__GETARGS _gfortran_iargc
+#else
+#define GETARG__GETARGS getarg_
+#define IARGC__GETARGS iargc_
+#endif
+
+extern "C" char * GETARG__GETARGS(const int &, char *, int);
+extern "C" int IARGC__GETARGS(void);
+
+/** 
+ * Get the number of arguments (argc)
+ */
+inline int GetNumberArguments(void) 
+{
+    return GETARG__GETARGS();
+}
+
+/**
+ * Get a particular argument from the command line
+ */
+inline void GetArgument(int i, char *arg, int length)
+{
+    IARGC__GETARGS(i, arg, length);
+}
+
+#endif // _GETARGUMENTS_HPP_
