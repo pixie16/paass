f5f34dfb2b84a8e180a9f086779c27d5aea4e69c
Krzysztof Miernik <k.a.miernik@gmail.com>
2013-07-26 10:50:53 -0400

    SSD processor for beta detection added
    Not included in the detector driver, old version, so
    may be incompatible with the new numbering scheme and correlator
    
    
    Former-commit-id: 5badac8aba63659da732bdd28da590b2182cbf6c

---
+++ include/SsdBetaProcessor.hpp
@@ -0,0 +1,28 @@
+/** \file SsdProcessor.h
+ *
+ * Header file for DSSD analysis
+ */
+
+#ifndef __SSD_PROCESSOR_H_
+#define __SSD_PROCESSOR_H_
+
+#include "EventProcessor.h"
+
+class DetectorSummary;
+class RawEvent;
+
+/**
+ * \brief Handles detectors of type dssd_front and dssd_back
+ */
+class SsdProcessor : public EventProcessor 
+{
+ private:
+    DetectorSummary *ssdSummary; ///< all detectors of type dssd_front
+
+ public:
+    SsdProcessor(); // no virtual c'tors
+    virtual void DeclarePlots(void) const;
+    virtual bool Process(RawEvent &event);
+};
+
+#endif // __SSD_POCESSOR_H_
