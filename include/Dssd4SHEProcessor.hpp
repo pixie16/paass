f3b1b2cc77821b5c9394a4d454e2bc866bec2067
Krzysztof Miernik <k.a.miernik@gmail.com>
2014-02-13 20:32:30 +0100

    Work on SheCorrelator
    Former-commit-id: 7a5d29d95b45aebd811fdd69cc16e3bc3a1b91d2

--- include/Dssd4SHEProcessor.hpp
+++ include/Dssd4SHEProcessor.hpp
@@ -9,6 +9,7 @@
 #include <utility>
 #include "EventProcessor.hpp"
 #include "RawEvent.hpp"
+#include "SheCorrelator.hpp"
 
 namespace dammIds { 
     namespace dssd4she {
@@ -17,6 +18,8 @@ namespace dammIds {
 
         const int D_DTIME_MISSING = 2;
         const int D_DE_MISSING = 3;
+
+        const int D_ENERGY_CORRELATED_SIDE = 4;
 
         const int DD_EVENT_POSITION_FROM_T = 10;
         const int DD_EVENT_POSITION_FROM_E = 11;
@@ -29,28 +32,42 @@ namespace dammIds {
 
         const int DD_DENERGY__DPOS_X_CORRELATED = 31; 
         const int DD_DENERGY__DPOS_Y_CORRELATED = 32; 
+
     }
 }
 
 
 class Dssd4SHEProcessor : public EventProcessor {
 public:
-    Dssd4SHEProcessor(double frontBackTimeWindow, double frontBackDE);
+    Dssd4SHEProcessor(double frontBackTimeWindow, 
+                      double highEnergyCut, 
+                      double lowEnergyCut, 
+                      int numFrontStrips, 
+                      int numBackStrips);
     virtual void DeclarePlots();
     virtual bool PreProcess(RawEvent &event);
     virtual bool Process(RawEvent &event);
 
-    /**Limit in seconds for the time difference between front and
-     * back to be correlated.*/
-    double frontBackTimeWindow_;
-
-    /**Limit in keV for the energy difference between front and
-     * back to be correlated.*/
-    double frontBackDE_;
-
 protected:
+    bool pickEventType(SheEvent& event);
+
+    SheCorrelator correlator;
+    /** Events matched based on energy (MaxEvent) **/
     std::vector<std::pair<ChanEvent*, ChanEvent*> > xyEventsEMatch_; 
+
+    /** Events matched based on timing correlation  **/
     std::vector<std::pair<ChanEvent*, ChanEvent*> > xyEventsTMatch_; 
+
+    /**Limit in seconds for the time difference between front and
+     * back to be correlated. Also to find Si Side detectors correlated
+     * events (escapes)*/
+    double timeWindow_;
+    /** Energy cut to differentiate high-energy events (fission?)
+     * from implantation and alpha decays (in keV) **/
+    double highEnergyCut_;
+    /** Energy cut to reject noise (in keV) **/
+    double lowEnergyCut_;
+
 };
 
 #endif 
--- src/SheCorrelator.cpp
+++ src/SheCorrelator.cpp
@@ -5,99 +5,69 @@
  */
 
 #include "SheCorrelator.hpp"
+#include <iostream>
 
 using namespace std;
+
+SheEvent::SheEvent() {
+    energy_ = -1.0;
+    time_= -1.0;
+    mwpc_= -1;
+    has_beam_= false;
+    has_veto_= false;
+    has_escape_ = false;
+    type_= unknown;
+}
+
+SheEvent::SheEvent(double energy, double time, int mwpc, 
+                   bool has_beam, bool has_veto, bool has_escape,
+                   SheEventType type) {
+    set_energy(energy);
+    set_time(time);
+    set_mwpc(mwpc);
+    set_beam(has_beam);
+    set_veto(has_veto);
+    set_escape(has_escape);
+    set_type(type);
+}
 
 
 SheCorrelator::SheCorrelator(int size_x, int size_y) {
     size_x_ = size_x;
     size_y_ = size_y;
-    pixels = new deque<SheEvent>*[size_x];
+    pixels_ = new deque<SheEvent>*[size_x];
     for(int i = 0; i < size_y; ++i)
-        pixels[i] = new deque<SheEvent>[size_y];
+        pixels_[i] = new deque<SheEvent>[size_y];
 }
 
 
 SheCorrelator::~SheCorrelator() {
     for(int i = 0; i < size_y_; ++i) {
-        delete[] pixels[i];
+        delete[] pixels_[i];
     }
-    delete[] pixels;
+    delete[] pixels_;
 }
 
 
 void SheCorrelator::add_event(SheEvent& event, int x, int y) {
-    bool high_energy = false;
-    if (event.energy > e_cut_)
-        high_energy = true;
+    cout << "Adding event set_type() " << event.get_type() << endl;
 
-    if (high_energy) {
-        if (event.has_veto) {
-            if (event.mwpc > 0) {
-                if (event.has_beam)
-                    event.type = lightIon;
-                else
-                    event.type = unknown;
-            } else {
-                if (event.has_beam)
-                    event.type = unknown;
-                else
-                    event.type = fission;
-            }
-        }
-        else {
-            if (event.mwpc > 0) {
-                if (event.has_beam)
-                    event.type = lightIon;
-                else
-                    event.type = fission;
-            } else {
-                event.type = fission;
-            }
-        }
-    }
-    else {
-        if (event.has_veto) {
-            if (event.mwpc > 0) {
-                if (event.has_beam)
-                    event.type = lightIon;
-                else
-                    event.type = unknown;
-            } else {
-                if (event.has_beam)
-                    event.type = lightIon;
-                else
-                    event.type = unknown;
-            }
-        }
-        else {
-            if (event.mwpc > 0) {
-                if (event.has_beam)
-                    event.type = heavyIon;
-                else
-                    event.type = decay;
-            } else {
-                event.type = decay;
-            }
-        }
-    }
-
-    if (event.type == heavyIon)
+    if (event.get_type() == heavyIon)
         save_and_flush_chain(x, y);
 
     pixels_[x][y].push_back(event);
 
-    if (event.type == fission)
+    if (event.get_type() == fission)
         save_and_flush_chain(x, y);
 
 }
 
 void SheCorrelator::save_and_flush_chain(int x, int y) {
+    cout << "Flush chain at " << x <<  " " << y << endl;
     for (deque<SheEvent>::iterator it = pixels_[x][y].begin();
          it != pixels_[x][y].end();
          ++it)
-        cout << it->energy << endl;
-
+        cout << it->get_energy() << endl;
     pixels_[x][y].clear();
 
 }
