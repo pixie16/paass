04f802983ed04f0f200beb7c8ea1f31e9050ac28
Krzysztof Miernik <k.a.miernik@gmail.com>
2014-02-14 12:23:32 +0100

    SheCorrelator beta version
    * low energy cut for alphas
    * human readable chains
    * saturation handling
    * front-back, dssd-si timing check
    * dE condition on low energy (<15 MeV) events
    * working correlator for dssd
    
    * CALIBRATION IS NEEDED!

--- include/Dssd4SHEProcessor.hpp
+++ include/Dssd4SHEProcessor.hpp
@@ -16,22 +16,30 @@ namespace dammIds {
         const int D_ENERGY_X = 0;
         const int D_ENERGY_Y = 1;
 
-        const int D_DTIME_MISSING = 2;
-        const int D_DE_MISSING = 3;
-
+        const int D_DTIME = 2;
+        const int D_MWPC_MULTI = 3;
         const int D_ENERGY_CORRELATED_SIDE = 4;
+        const int D_DTIME_SIDE = 5;
 
-        const int DD_EVENT_POSITION_FROM_T = 10;
+        const int DD_EVENT_POSITION = 10;
         const int DD_EVENT_POSITION_FROM_E = 11;
+        const int DD_IMPLANT_POSITION = 12;
+        const int DD_DECAY_POSITION = 13;
+        const int DD_LIGHT_POSITION = 14;
+        const int DD_UNKNOWN_POSITION = 15;
+        const int DD_FISSION_POSITION = 16;
 
-        const int DD_EVENT_ENERGY__X_POSITION = 15;
-        const int DD_EVENT_ENERGY__Y_POSITION = 16;
+        const int DD_EVENT_ENERGY__X_POSITION = 17;
+        const int DD_EVENT_ENERGY__Y_POSITION = 18;
+        const int DD_MAXEVENT_ENERGY__X_POSITION = 19;
+        const int DD_MAXEVENT_ENERGY__Y_POSITION = 20;
 
-        const int DD_ENERGY__POSX_T_MISSING = 21;
-        const int DD_ENERGY__POSY_T_MISSING = 22; 
+        const int DD_FRONTE__BACKE = 21;
+        const int DD_ENERGY__POSX_T_MISSING = 22;
+        const int DD_ENERGY__POSY_T_MISSING = 23; 
 
-        const int DD_DENERGY__DPOS_X_CORRELATED = 31; 
-        const int DD_DENERGY__DPOS_Y_CORRELATED = 32; 
+        const int DD_DENERGY__DPOS_X_CORRELATED = 41; 
+        const int DD_DENERGY__DPOS_Y_CORRELATED = 42; 
 
     }
 }
@@ -40,8 +48,10 @@ namespace dammIds {
 class Dssd4SHEProcessor : public EventProcessor {
 public:
     Dssd4SHEProcessor(double frontBackTimeWindow, 
+                      double deltaEnergy,
                       double highEnergyCut, 
                       double lowEnergyCut, 
+                      double fisisonEnergyCut, 
                       int numFrontStrips, 
                       int numBackStrips);
     virtual void DeclarePlots();
@@ -51,7 +61,7 @@ public:
 protected:
     bool pickEventType(SheEvent& event);
 
-    SheCorrelator correlator;
+    SheCorrelator correlator_;
     /** Events matched based on energy (MaxEvent) **/
     std::vector<std::pair<ChanEvent*, ChanEvent*> > xyEventsEMatch_; 
 
@@ -62,11 +72,20 @@ protected:
      * back to be correlated. Also to find Si Side detectors correlated
      * events (escapes)*/
     double timeWindow_;
+
+    /**Limit in keV of difference between front and back events to
+     * be considered a good event */
+    double deltaEnergy_;
+
     /** Energy cut to differentiate high-energy events (fission?)
      * from implantation and alpha decays (in keV) **/
     double highEnergyCut_;
-    /** Energy cut to reject noise (in keV) **/
+
+    /** Low Energy cut for interesting alphas (in keV) **/
     double lowEnergyCut_;
+
+    /** Fission Energy cut (in keV) **/
+    double fissionEnergyCut_;
 
 };
 
