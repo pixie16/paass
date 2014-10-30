3039328213bf03134a9bf714b367e1bc242ebea8
S.V. Paulauskas <stanpaulauskas@gmail.com>
2014-10-29 23:04:37 -0400

    removing the Vandle and Scint ROOT classes

--- src/WaveformAnalyzer.cpp
+++ src/WaveformAnalyzer.cpp
@@ -1,5 +1,5 @@
 /** \file WaveformAnalyzer.cpp
- *\brief Preliminary waveoform analysis
+ *\brief Preliminary waveform analysis
  *
  *Does preliminary waveform analysis on traces. The parameters set here
  *will be used for the high resolution timing algorithms to do their thing.
@@ -14,52 +14,41 @@
 
 #include <cmath>
 
-#include "FittingAnalyzer.hpp"
 #include "WaveformAnalyzer.hpp"
 
 using namespace std;
 
-//********** WaveformAnalyzer **********
-WaveformAnalyzer::WaveformAnalyzer() : TraceAnalyzer()
-{
+WaveformAnalyzer::WaveformAnalyzer() : TraceAnalyzer() {
     name = "Waveform";
 }
 
-
-//********** DeclarePlots **********
-void WaveformAnalyzer::DeclarePlots(void) const
-{
-}
-
-
-//********** Analyze **********
 void WaveformAnalyzer::Analyze(Trace &trace,
-			       const string &detType,
-			       const string &detSubtype) {
+			       const std::string &detType,
+			       const std::string &detSubtype) {
     TraceAnalyzer::Analyze(trace, detType, detSubtype);
 
     if(detType == "vandleSmall" || detType == "vandleBig"
        || detType == "liquid_scint" || detType == "pulser"
        || detType == "tvandle") {
 
-	if(trace.HasValue("saturation")) {
-	    EndAnalyze();
-	    return;
-	}
+        if(trace.HasValue("saturation")) {
+            EndAnalyze();
+            return;
+        }
 
-	unsigned int waveformLow = Globals::get()->waveformLow();
-	unsigned int waveformHigh = Globals::get()->waveformHigh();
-	unsigned int startDiscrimination = Globals::get()->discriminationStart();
-	unsigned int maxPos = trace.FindMaxInfo();
+        unsigned int waveformLow = Globals::get()->waveformLow();
+        unsigned int waveformHigh = Globals::get()->waveformHigh();
+        unsigned int startDiscrimination = Globals::get()->discriminationStart();
+        unsigned int maxPos = trace.FindMaxInfo();
 
-	double qdc = trace.DoQDC(maxPos-waveformLow,
+        double qdc = trace.DoQDC(maxPos-waveformLow,
                                  waveformHigh+waveformLow);
 
-	trace.InsertValue("qdcToMax", qdc/trace.GetValue("maxval"));
+        trace.InsertValue("qdcToMax", qdc/trace.GetValue("maxval"));
 
-	if(detSubtype == "liquid")
-	    trace.DoDiscrimination(startDiscrimination,
-	 			   waveformHigh - startDiscrimination);
+        if(detSubtype == "liquid")
+            trace.DoDiscrimination(startDiscrimination,
+                    waveformHigh - startDiscrimination);
     } //if(detType
     EndAnalyze();
 }
