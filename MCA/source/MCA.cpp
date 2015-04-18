6bcdffda84b322d3f1128f0ee62b8ff2f9dfa8b5
Karl Smith <ksmit218@utk.edu>
2015-04-18 10:58:03 -0400

    Added MCA::Stop method.
    Added a stop method to allow for the user to stop the MCA during
    acquisition.

--- MCA/source/MCA.cpp
+++ MCA/source/MCA.cpp
@@ -7,32 +7,50 @@
 #include "Display.h"
 #include "Utility.h"
 
+/**The MCA is initialized and run for the specified duration or until a
+ * stop command is received. At specific intervals the MCA output is
+ * updated via MCA::StoreData().
+ *
+ * \parma[in] duration Amount of time to run the MCA.
+ */
 void MCA::Run(float duration) {
+	//Reset stop flag in case it was used for termination.
+	_stop = false;
 
 	//Start the pixie histogram
 	_pif->StartHistogramRun();
 
 	float runTime = 0;
-	usleep(100);
-	while (runTime < duration) {
+
+	//Loop until we reach the run duration or a stop is received.
+	while (runTime < duration || !_stop) {
 		sleep(2);
+		//Update run time
 		runTime += usGetDTime() / 1.0e6;
 		std::cout << "|" << std::fixed << std::setprecision(2) << runTime  << " s |\r" << std::flush; 
+
+		//Check if run is still ok
 		if (!_pif->CheckRunStatus()) {
 			std::cout << Display::ErrorStr("Run TERMINATED") << std::endl;
 			break;
 		}
+
+		//Store the MCA data via the inherited method StoreData()
 		for (int mod = 0;mod < _pif->GetNumberCards(); mod++) {
 			for (int ch = 0; ch < _pif->GetNumberChannels(); ch++) {
 				StoreData(mod, ch);
 			}
 		}
+		//Flush the data to disk.
 		Flush();
 	}
 
+	//End the run
+	_pif->EndRun();
+
+	//Display run completion information.
 	runTime += usGetDTime() / 1.0e6;
 	std::cout << std::endl;
-	_pif->EndRun();
 	Display::LeaderPrint("Run finished");
 	std::cout << Display::OkayStr() << std::endl;
 	Display::LeaderPrint("Total running time:");
@@ -41,4 +59,9 @@ void MCA::Run(float duration) {
 	//Uset cout flags
 	std::cout.unsetf(std::ios_base::floatfield);
 	std::cout.precision(6);
+
+}
+
+void MCA::Stop() {
+	_stop = true;
 }
