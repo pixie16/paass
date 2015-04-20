eaec733bda124aad3c14ba717948eeaab3af0ade
Cory Thornsberry <cthornsb@vols.utk.edu>
2015-04-20 17:35:08 -0400

    Fixed poll2 network communication
    Fixed a problem with poll2_socket which resulted in
    clients not being able to receive data which was
    broadcast over the network by poll2.
    
    Moved all server and client commands into new
    classes Server and Client respectively.
    
    The Poll class now contains a pointer to a Client
    object which is used for network communication.
    
    NOTE: If poll2 uses a server, clients are not able
    to read packets which it broadcasts. Modifying
    poll2 to use a client fixes this problem. Future
    programs which are intended to communicate with
    poll2 must use the server class.
    
    Poll2 now broadcasts, in addition to output file
    information packets, signals. At this time, poll2
    transmits the following two signals
     $CLOSE_FILE - Poll has closed the current file.
     $KILL_SOCKET - Poll is closing and will close
      the communication socket.
    
    Added a new executable named "listener". Listener
    should be used to monitor data packets which are
    being broadcast by poll2. Listener will receive,
    decode, and display contents of the poll2 packets.

---
+++ Poll/source/listener.cpp
@@ -0,0 +1,110 @@
+/** \file listener.cpp
+  * 
+  * \brief Receives and decodes packets broadcast by poll2
+  * 
+  * \author Cory R. Thornsberry
+  * 
+  * \date April 20th, 2015
+*/
+
+#include <iostream>
+#include <string.h>
+
+#include "poll2_socket.h"
+
+int main(){
+	char buffer[1024];
+	Server poll_server;
+	
+	int total_size, spillID, buffSize;
+	int end_packet_flag;
+	std::streampos file_size;
+	
+	size_t size_of_int; // The first byte is always the size of an integer on the sending machine
+	size_t size_of_spos; // The second byte is always the size of a std::streampos type on the sending machine
+	
+	std::cout << " Size of integer on this machine: " << sizeof(int) << " bytes\n";
+	std::cout << " Size of streampos on this machine: " << sizeof(std::streampos) << " bytes\n";
+	
+	size_t min_pack_size = 2 + 2*sizeof(int);
+	std::cout << " Minimum packet size: " << min_pack_size << " bytes\n\n";
+	
+	bool first_packet = true;
+	if(poll_server.Init(5555)){
+		while(true){
+			std::cout << " recv: " << poll_server.RecvMessage(buffer, 1024) << " bytes\n";
+
+			if(strcmp(buffer, "$CLOSE_FILE") == 0){
+				std::cout << "  Received CLOSE_FILE flag...\n\n";
+				continue;
+			}
+			else if(strcmp(buffer, "$KILL_SOCKET") == 0){
+				std::cout << "  Received KILL_SOCKET flag...\n\n";
+				break;
+			}
+
+			// decode the packet
+			if(first_packet){
+				size_of_int = (size_t)buffer[0];
+				size_of_spos = (size_t)buffer[1];
+		
+				if(size_of_int != sizeof(int) || size_of_spos != sizeof(std::streampos)){ 
+					std::cout << "  Warning! basic type size on remote machine does not match local size\n"; 
+					std::cout << "  Size of integer on remote machine: " << size_of_int << " bytes\n";
+					std::cout << "  Size of streampos on remote machine: " << size_of_spos << " bytes\n\n";
+					break;
+				}
+				
+				first_packet = false;
+			}
+		
+			// The third byte is the start of an integer specifying the total length of the packet
+			// (in bytes) including itself and the first two size bytes.
+			memcpy((char *)&total_size, &buffer[2], size_of_int);
+		
+			if(total_size <= min_pack_size){
+				// Below is the buffer packet structure
+				// ------------------------------------
+				// 1 byte size of integer (may not be the same on a different machine)
+				// 1 byte size of streampos (may not be the same on a different machine)
+				// 4 byte packet length (inclusive, also includes the end packet flag)
+				// 4 byte begin packet flag (0xFFFFFFFF)
+				std::cout << "  Null packet...\n\n";
+			}
+			else{
+				// Below is the buffer packet structure
+				// ------------------------------------
+				// 1 byte size of integer (may not be the same on a different machine)
+				// 1 byte size of streampos (may not be the same on a different machine)
+				// 4 byte packet length (inclusive, also includes the end packet flag)
+				// x byte file path (no size limit)
+				// 8 byte file size streampos (long long)
+				// 4 byte spill number ID (unsigned int)
+				// 4 byte buffer size (unsigned int)
+				// 4 byte end packet flag (0xFFFFFFFF)
+				size_t fname_size = total_size - (2 + 4*size_of_int) - size_of_spos; // Size of the filename (in bytes)
+				char fname[fname_size+1];
+	
+				unsigned int index = 2  + size_of_int;
+				memcpy(fname, (char *)&buffer[index], fname_size); index += fname_size; // Copy the file name
+				memcpy((char *)&file_size, (char *)&buffer[index], size_of_spos); index += size_of_spos; // Copy the file size
+				memcpy((char *)&spillID, (char *)&buffer[index], size_of_int); index += size_of_int; // Copy the spill ID
+				memcpy((char *)&buffSize, (char *)&buffer[index], size_of_int); index += size_of_int; // Copy the buffer size
+				memcpy((char *)&end_packet_flag, (char *)&buffer[index], size_of_int); // Copy the end packet flag
+				fname[fname_size] = '\0'; // Terminate the filename string
+				
+				std::cout << "  Packet length: " << total_size << " bytes\n";
+				//printf("  Poll2 filename: %s", fname);
+				std::cout << "  Poll2 filename: " << fname << "\n";
+				std::cout << "  Total file size: " << file_size << " bytes\n";
+				std::cout << "  Spill number ID: " << spillID << "\n";
+				std::cout << "  Buffer size: " << buffSize << " words\n";
+				std::cout << "  End packet: " << end_packet_flag << "\n\n";				
+			}
+		}
+	}
+	else{ return 1; }
+	poll_server.Close();
+
+	return 0;
+}
--- Poll/source/poll2_socket.cpp
+++ Poll/source/poll2_socket.cpp
@@ -1,189 +1,117 @@
+/** \file poll2_socket.cpp
+  * 
+  * \brief Provides network connectivity for poll2
+  * 
+  * \author Cory R. Thornsberry
+  * 
+  * \date April 20th, 2015
+  * 
+  * This file contains classes used by poll2 in order to send packets over the
+  * network. These packets are sent by poll using the Server class and may be
+  * read by any program using the Client class or operating a socket on the
+  * same port.
+*/
+
 #include "poll2_socket.h"
 
-struct sockaddr_in cli_sa, cli_addr, serv_sa;
-bool cli_init(false), serv_init(false);
-int cli_sock, serv_sock;
-socklen_t cli_size, serv_size;
+#include <iostream>
+#include <string>
+#include <sstream>
 
-bool client_connected = false;
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
 
-bool init_client(const char* address_, int port_){
-	if(cli_init){ return false; }
+/////////////////////////////////////////////////////////////////////
+// class Server
+/////////////////////////////////////////////////////////////////////
 
-	if((cli_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){ 
-		return false;
-	}
-	
-	bzero(&cli_sa, sizeof cli_sa);
-	cli_sa.sin_family = AF_INET;
+bool Server::Init(int port_){
+	if(init){ return false; }
 
-	int numbers[4];
-	std::string temp;
-	unsigned int count = 0, index = 0;
-	
-	while(true){
-		if(address_[count] == '.'){
-			numbers[index] = atoi(temp.c_str());
-			temp = "";
-			index++;
-		}
-		else{ temp += address_[count]; }
-		count++;
-	
-		if(address_[count] == '\0' || index >= 4){ 
-			numbers[index] = atoi(temp.c_str());
-			break; 
-		}
-	}
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sock < 0){ return false; } // failed to open socket
 
-	cli_sa.sin_port = htons(port_);
-	cli_sa.sin_addr.s_addr = htonl((((((numbers[0] << 8) | numbers[1]) << 8) | numbers[2]) << 8) | numbers[3]);		
+	length = sizeof(serv);
+	bzero(&serv, length);
 
-	/*if(connect(cli_sock, (struct sockaddr *)&cli_sa, sizeof cli_sa) < 0){
-		close(cli_sock);
-		return false;
-	}*/
+	serv.sin_family = AF_INET;
+	serv.sin_addr.s_addr = INADDR_ANY;
+	serv.sin_port = htons(port_);
 
-	cli_size = sizeof(cli_sa);
+	if(bind(sock, (struct sockaddr *)&serv, length) < 0){ return false; } // failed to bind to port
 
-	return cli_init = true;
+	fromlen = sizeof(struct sockaddr_in);
+
+	return init = true;
 }
 
-/*bool client_send_message(char *message_, size_t length_){
-	if(!cli_init){ return false; }
-	
-	if(write(cli_sock, message_, length_) < 0){
-		return false; 
-	}
+int Server::RecvMessage(char *message_, size_t length_){
+	if(!init){ return -1; }
 
-	return true;
-}
-
-bool client_send_message(std::string message_){
-	char input[message_.size()];
-	for(size_t i = 0; i < message_.size(); i++){
-		input[i] = message_[i];
-	}
-	
-	return client_send_message(input, message_.size());
-}*/
-
-int client_recv_message(char *message_ , size_t length_){
-	if(!cli_init){ return false; }
-
-	// Clear the message
+	// Zero the message
 	bzero(message_, length_);
 
-	/*if(read(cli_sock, message_, length_) < 0){
-		return false;
-	}*/
-	
-	return recvfrom(cli_sock, message_, length_, 0, (struct sockaddr *) &cli_sa, &cli_size);
+	int nbytes = (int)recvfrom(sock, message_, length_, 0, (struct sockaddr *)&from, &fromlen);
+	return nbytes;
 }
 
-int client_recv_message(std::string &message, size_t max_length_){
-	std::stringstream stream;
-	char str[max_length_];
-	int output = client_recv_message(str, max_length_);
-	
-	stream << str;
-	message = stream.str();
-	return output;
+int Server::SendMessage(char *message_, size_t length_){
+	if(!init){ return -1; }
+
+	return (int)sendto(sock, message_, length_, 0, (struct sockaddr *)&from, fromlen);
 }
 
-bool close_client(){
-	if(!cli_init){ return false; }
-	
-	close(cli_sock);
-	return true;
+void Server::Close(){
+	if(!init){ return; }
+
+	close(sock);
 }
 
-bool init_server(int portNum_){
-	if(serv_init){ return false; }
+/////////////////////////////////////////////////////////////////////
+// class Client
+/////////////////////////////////////////////////////////////////////
 
-	if((serv_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-		return false;
-	}
-	
-	bzero(&serv_sa, sizeof serv_sa);
-	serv_sa.sin_family = htonl(AF_INET);
-	serv_sa.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_sa.sin_port = htons(portNum_);
-	
-	if(bind(serv_sock, (struct sockaddr *) &serv_sa, sizeof(serv_sa)) < 0){ 
-		close(serv_sock);
-		return false;
-	}
-	
-	//listen(serv_sock, 5);
-	
-	serv_size = sizeof(serv_sa);
-	
-	return serv_init = true;
+bool Client::Init(const char *address_, int port_){
+	if(init){ return false; }
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sock < 0){ return false; } // failed to open socket
+
+	serv.sin_family = AF_INET;
+	hp = gethostbyname(address_);
+	if(!hp){ return false; } // failed to resolve hostname
+
+	bcopy((char *)hp->h_addr, (char *)&serv.sin_addr, hp->h_length);
+	serv.sin_port = htons(port_);
+	length = sizeof(struct sockaddr_in);
+
+	return init = true;
 }
 
-/*bool run_server(){
-	if(!serv_init){ return false; }
+int Client::RecvMessage(char *message_, size_t length_){
+	if(!init){ return -1; }
 
-	char buffer[512];
-	char reply[19] = "I got your message";
-	size_t size;
-	int nbytes;
-
-	unsigned int cli_addr_len = sizeof(cli_addr);
-	size = sizeof (serv_sa);
-	
-	while(true){
-		cli_sock = accept(serv_sock, (struct sockaddr *) &cli_addr, &cli_addr_len);
-		if(cli_sock < 0){
-			continue;
-		}
-		
-		while(true){
-			server_recv_message(buffer, 512)
-			
-			// Catch disconnect signal
-			if(strcmp(buffer, "$DISCONNECT") == 0){
-				break;
-			}
-			
-			printf("CLIENT: %s\n",buffer);
-		
-			server_send_message(reply, 19)
-		}
-	}
-	
-	close(cli_sock);
-	
-	return true;
-}*/
-
-int server_send_message(char *message_, size_t length_){
-	if(!serv_init){ return false; }
-
-	/*if(write(serv_sock, message_, length_) < 0){
-		return false; 
-	}*/
-
-	return sendto(serv_sock, message_, length_, 0, (struct sockaddr *) &serv_sa, serv_size);
-}
-
-int server_recv_message(char *message_ , size_t length_){
-	if(!serv_init){ return false; }
-
-	// Clear the message
+	// Zero the message
 	bzero(message_, length_);
 
-	/*if(read(serv_sock, message_, length_) < 0){
-		return false;
-	}*/
-	
-	return recvfrom(serv_sock, message_, length_, 0, (struct sockaddr *) &serv_sa, &serv_size);
+	return (int)recvfrom(sock, message_, length_, 0, (struct sockaddr *)&from, &length);
 }
 
-bool close_server(){
-	if(!serv_init){ return false; }
-	
-	close(serv_sock);
-	return true;
+int Client::SendMessage(char *message_, size_t length_){
+	if(!init){ return -1; }
+
+	return (int)sendto(sock, message_, length_, 0, (const struct sockaddr *)&serv, length);
+}
+
+void Client::Close(){
+	if(!init){ return; }
+
+	close(sock);
 }
