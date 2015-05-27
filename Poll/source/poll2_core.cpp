pixie16/paass

commit 2ed94ec6b67fde4f45afbd93d45fd3254067e127

Fixed shared-memory (SHM) readout
poll2_core 1.3.01

Added a new command to poll2 named 'shm' (acq) which
activates shared-memory mode. SHM may be used either
with or without output to a data file. It is active
whenever the acquisition system is running, i.e.
startacq.

If shm_mode is active, Poll::write_data will now
chop the data up into chunks of 10,000 words (max
size to send onto the network socket) and will
then broadcast the chunks onto the network using
poll2_core.

poll2_socket v1.1.01

Added Server::Select method which may be used to
check whether or not there is any data waiting
on the socket. The Server class may now be
initialized with a timeout. This timeout is only
used for the ::Select method and does not affect
the normal Send/RecvMessage methods.

hribf_buffers.cpp

Fixed the passing of a string in place of a const
char * when opening a file using ifstream.

poll2.cpp

Fixed an issue with integer division which was
causing the FIFO threshold to always take the
same value. The -t command line argument now
works as expected.

Poll/source/poll2_core.cpp
@@ -102,6 +102,7 @@ Poll::Poll(){
 	show_module_rates = false;
 	zero_clocks = false;
 	debug_mode = false;
+	shm_mode = false;
 	init = false;
 
 	// Options relating to output data file
@@ -262,26 +263,57 @@ bool Poll::synch_mods(){
 }
 
 int Poll::write_data(word_t *data, unsigned int nWords){
-	// Open an output file if needed
-	if(!output_file.IsOpen()){
-		open_output_file();
+	if(shm_mode){ // Broadcast the spill onto the network
+		char shm_data[40008]; // 40 kB packets of data
+		char *data_l = (char *)data; // Local pointer to the data array
+		unsigned int num_net_chunks = nWords / 10000;
+		unsigned int num_net_remain = nWords % 10000;
+		if(num_net_remain != 0){ num_net_chunks++; }
+		
+		unsigned int net_chunk = 1;
+		unsigned int words_bcast = 0;
+		if(debug_mode){ std::cout << " debug: Splitting " << nWords << " words into network spill of " << num_net_chunks << " chunks (fragment = " << num_net_remain << " words)\n"; }
+		
+		while(words_bcast < nWords){
+			if(nWords - words_bcast > 10000){ // Broadcast the spill chunks
+				memcpy(&shm_data[0], (char *)&net_chunk, 4);
+				memcpy(&shm_data[4], (char *)&num_net_chunks, 4);
+				memcpy(&shm_data[8], &data_l[words_bcast*4], 40000);
+				client->SendMessage(shm_data, 40008);
+				words_bcast += 10000;
+			}
+			else{ // Broadcast the spill remainder
+				memcpy(&shm_data[0], (char *)&net_chunk, 4);
+				memcpy(&shm_data[4], (char *)&num_net_chunks, 4);
+				memcpy(&shm_data[8], &data_l[words_bcast*4], (nWords-words_bcast)*4);
+				client->SendMessage(shm_data, (nWords - words_bcast + 2)*4);
+				words_bcast += nWords-words_bcast;
+			}
+			net_chunk++;
+		}
+	}
+	else{ // Broadcast a spill notification to the network
+		char packet[output_file.GetPacketSize()];
+		int packet_size = output_file.BuildPacket(packet);
 	}
 
-	// Broadcast a spill notification to the network
-	char packet[output_file.GetPacketSize()];
-	int packet_size = output_file.BuildPacket(packet);
-	client->SendMessage(packet, packet_size);
-
-	// Handle the writing of buffers to the file
-	std::streampos current_filesize = output_file.GetFilesize();
-	if(current_filesize + (std::streampos)(4*nWords + 65552) > MAX_FILE_SIZE){
-		// Adding nWords plus 2 EOF buffers to the file will push it over MAX_FILE_SIZE.
-		// Open a new output file instead
-		std::cout << sys_message_head << "Current filesize is " << current_filesize + (std::streampos)65552 << " bytes\n";
-		close_output_file();
-		open_output_file();
+	if(record_data){
+		// Open an output file if needed
+		if(!output_file.IsOpen()){
+			open_output_file();
+		}
+	
+		// Handle the writing of buffers to the file
+		std::streampos current_filesize = output_file.GetFilesize();
+		if(current_filesize + (std::streampos)(4*nWords + 65552) > MAX_FILE_SIZE){
+			// Adding nWords plus 2 EOF buffers to the file will push it over MAX_FILE_SIZE.
+			// Open a new output file instead
+			std::cout << sys_message_head << "Current filesize is " << current_filesize + (std::streampos)65552 << " bytes\n";
+			close_output_file();
+			open_output_file();
+		}
+		return output_file.Write((char*)data, nWords);
 	}
-	return output_file.Write((char*)data, nWords);
 
 	return -1;
 }
@@ -293,6 +325,7 @@ void Poll::help(){
 	std::cout << "   stop             - Stop data acqusition and stop recording data to disk\n";	
 	std::cout << "   startacq         - Start data acquisition\n";
 	std::cout << "   stopacq          - Stop data acquisition\n";
+	std::cout << "   acq (shm)        - Run in \"shared-memory\" mode\n";
 	std::cout << "   spill (hup)      - Force dump of current spill\n";
 	std::cout << "   prefix [name]    - Set the output filename prefix (default='run_#.ldf')\n";
 	std::cout << "   fdir [path]      - Set the output file directory (default='./')\n";
@@ -459,7 +492,7 @@ void Poll::command_control(){
 		if(c == '\004'){ break; } // ctrl + c
 		else if(c == '\n' || c == '\r'){
 			cmd_ready = true;
-		}
+		}write_data
 		else if(c == '\033'){
 			read(STDIN_FILENO, &c, 1); // skip the '['
 			read(STDIN_FILENO, &c, 1);
@@ -513,6 +546,7 @@ void Poll::command_control(){
 				std::cout << "   Acq starting    - " << yesno(start_acq) << std::endl;
 				std::cout << "   Acq stopping    - " << yesno(stop_acq) << std::endl;
 				std::cout << "   Acq running     - " << yesno(acq_running) << std::endl;
+				std::cout << "   Shared memory   - " << yesno(shm_mode) << std::endl;
 				std::cout << "   Write to disk   - " << yesno(record_data) << std::endl;
 				std::cout << "   File open       - " << yesno(output_file.IsOpen()) << std::endl;
 				std::cout << "   Rebooting       - " << yesno(do_reboot) << std::endl;
@@ -540,6 +574,16 @@ void Poll::command_control(){
 			} 
 			else if(cmd == "stopacq" || cmd == "stopvme"){ // Tell POLL to stop data acquisition
 				StopAcq();
+			}
+			else if(cmd == "acq" || cmd == "shm"){ // Toggle "shared-memory" mode
+				if(shm_mode){
+					std::cout << sys_message_head << "Toggling shared-memory mode OFF\n";
+					shm_mode = false;
+				}
+				else{
+					std::cout << sys_message_head << "Toggling shared-memory mode ON\n";
+					shm_mode = true;
+				}
 			}
 			else if(cmd == "reboot"){ // Tell POLL to attempt a PIXIE crate reboot
 				if(do_MCA_run){ std::cout << sys_message_head << "Warning! Cannot reboot while MCA is running\n"; }
@@ -1122,9 +1166,7 @@ void Poll::run_control(){
 				} //End loop over modules for reading FIFO
 
 				//We have read the FIFO now we write the data	
-				if(record_data){ 
-					write_data(fifoData, dataWords); 
-				}
+				write_data(fifoData, dataWords); 
 				
 				//Get the length of the spill
 				double spillTime = usGetTime(startTime);

Poll/include/poll2_core.h
@@ -10,9 +10,9 @@
   *
   * \author Cory R. Thornsberry
   * 
-  * \date May 6th, 2015
+  * \date May 11th, 2015
   * 
-  * \version 1.3.00
+  * \version 1.3.01
 */
 
 
@@ -25,8 +25,8 @@
 #include "hribf_buffers.h"
 #define maxEventSize 4095 // (0x1FFE0000 >> 17)
 
-#define POLL2_CORE_VERSION "1.3.00"
-#define POLL2_CORE_DATE "May 6th, 2015"
+#define POLL2_CORE_VERSION "1.3.01"
+#define POLL2_CORE_DATE "May 11th, 2015"
 
 typedef PixieInterface::word_t word_t;
 typedef word_t eventdata_t[maxEventSize];
@@ -94,6 +94,7 @@ class Poll{
 	bool show_module_rates; //
 	bool zero_clocks; //
 	bool debug_mode; //
+	bool shm_mode; //
 	bool init; //
 
 	// Options relating to output data file

Poll/include/poll2_socket.h
@@ -17,8 +17,8 @@
 
 #include <netinet/in.h>
 
-#define POLL2_SOCKET_VERSION "1.1.00"
-#define POLL2_SOCKET_DATE "April 22nd, 2015"
+#define POLL2_SOCKET_VERSION "1.1.01"
+#define POLL2_SOCKET_DATE "May 11th, 2015"
 
 class Server{
   private:
@@ -28,14 +28,20 @@ class Server{
 	struct sockaddr_in from;
 	bool init;
 
+	int to_sec, to_usec;
+	fd_set readfds, masterfds;
+	struct timeval timeout;
+
   public:
 	Server(){ init = false; }
   
 	~Server(){ Close(); }
 
+	int Get(){ return sock; }
+
 	/** Initialize the serv object and open a specified port. Returns false if the 
 	  * socket fails to open or the socket fails to bind and returns true otherwise. */
-	bool Init(int port_);
+	bool Init(int port_, int sec_=10, int usec_=0);
 
 	/** Receive a message from the socket. Returns the number of bytes received. Returns
 	  * -1 if the receive fails or if the object was not initialized. */
@@ -44,6 +50,8 @@ class Server{
 	/** Send a message to the socket. Returns the number of bytes sent. Returns
 	  * -1 if the send fails or if the object was not initialized. */
 	int SendMessage(char *message_, size_t length_);
+
+	bool Select(int &retval);
 
 	/// Close the socket.
 	void Close();
@@ -61,7 +69,9 @@ class Client{
 	Client(){ init = false; }
 	
 	~Client(){ Close(); }
-  	
+  
+  	int Get(){ return sock; }
+  
 	/** Initialize the client object and open a specified address and port. Returns false 
 	  * if the socket fails to open or the address is unresolved and returns true otherwise. */
 	bool Init(const char *address_, int port_);

Poll/source/hribf_buffers.cpp
@@ -845,12 +845,12 @@ bool PollOutputFile::OpenNewFile(std::string title_, int run_num_, std::string &
 
 	std::stringstream filename;
 	filename << output_directory << prefix << "_" << run_num_ << ".ldf";
-	std::ifstream input_file(filename.str());
+	std::ifstream input_file(filename.str().c_str());
 	if (input_file.is_open()) {
 		input_file.close();
 		return false;
 	}
-	output_file.open(filename.str(), std::ios::binary);
+	output_file.open(filename.str().c_str(), std::ios::binary);
 	if(!output_file.is_open() || !output_file.good()){
 		output_file.close();
 		return false;

Poll/source/poll2.cpp
@@ -120,9 +120,9 @@ int main(int argc, char *argv[]){
 	if(valid_opt[3].is_active){ poll.insert_wall_clock = false; }
 	if(valid_opt[4].is_active){ poll.show_module_rates = true; }
 	if(valid_opt[5].is_active){ 
-		threshPercent = atoi(valid_opt[7].value.c_str()); 
+		threshPercent = atoi(valid_opt[5].value.c_str()); 
 		if(threshPercent <= 0){ 
-			std::cout << "Warning! failed to set threshold level. Using default of 50%\n";
+			std::cout << Display::WarningStr("Warning!") << " failed to set threshold level. Using default of 50%\n";
 			threshPercent = 50; 
 		}
 	}
@@ -153,7 +153,7 @@ int main(int argc, char *argv[]){
 	std::cout << "\n POLL2 v" << POLL2_CORE_VERSION << "\n"; 
 	std::cout << " ==  ==  ==  ==  == \n\n"; 
 	
-	poll.threshWords = EXTERNAL_FIFO_LENGTH * threshPercent / 100;
+	poll.threshWords = EXTERNAL_FIFO_LENGTH * threshPercent / 100.0;
 	std::cout << "Using FIFO threshold of " << poll.threshWords << " words\n";
 	
 #ifdef PIF_REVA

Poll/source/poll2_socket.cpp
@@ -32,7 +32,7 @@
 // class Server
 /////////////////////////////////////////////////////////////////////
 
-bool Server::Init(int port_){
+bool Server::Init(int port_, int sec_, int usec_){
 	if(init){ return false; }
 
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -48,6 +48,9 @@ bool Server::Init(int port_){
 	if(bind(sock, (struct sockaddr *)&serv, length) < 0){ return false; } // failed to bind to port
 
 	fromlen = sizeof(struct sockaddr_in);
+
+	to_sec = sec_;
+	to_usec = usec_;
 
 	return init = true;
 }
@@ -66,6 +69,20 @@ int Server::SendMessage(char *message_, size_t length_){
 	if(!init){ return -1; }
 
 	return (int)sendto(sock, message_, length_, 0, (struct sockaddr *)&from, fromlen);
+}
+
+bool Server::Select(int &retval){
+	timeout.tv_sec = to_sec; // Set timeout to sec_
+	timeout.tv_usec = to_usec;
+
+	FD_ZERO(&masterfds);
+	FD_SET(sock, &masterfds);
+	memcpy(&readfds, &masterfds, sizeof(fd_set));
+
+	retval = select(sock+1, &readfds, NULL, NULL, &timeout);
+	if(FD_ISSET(sock, &readfds)){ return true; }
+
+	return false;
 }
 
 void Server::Close(){

commit faef718ac7278483e0d254bd48f66dcd3790ca8c

Corrected broadcast of spill information.
The spill info broadcast was broken in commit
2ed94ec6b67fde4f45afbd93d45fd3254067e127.

Poll/source/poll2_core.cpp
@@ -291,6 +291,7 @@ void Poll::broadcast_data(word_t *data, unsigned int nWords) {
 	else{ // Broadcast a spill notification to the network
 		char packet[output_file.GetPacketSize()];
 		int packet_size = output_file.BuildPacket(packet);
+		client->SendMessage(packet, packet_size);
 	}
 }
 