48167f856e56572d1cc13cd05aa9774619e7722a
Cory R. Thornsberry <corythornsberry@gmail.com>
2015-08-28 16:46:32 -0400

    Added files to PixieCore
    Added new PixieCore lib files ChannelEvent,
    Unpacker, and ScanMain. These files are originally
    from RootPixieScan and are intended to be used to
    allow the user to unpack pixie16 data from poll.
    
    Added ./lib to the LIBRARY_PATH env variable.

---
+++ Core/source/ChannelEvent.cpp
@@ -0,0 +1,134 @@
+#include <cmath>
+
+#include "ChannelEvent.hpp"
+
+ChannelEvent::ChannelEvent(){
+	xvals = NULL;
+	yvals = NULL;
+	Clear();
+}
+
+void ChannelEvent::reserve(const size_t &size_){
+	if(size != 0){ return; }
+	size = size_;
+	xvals = new float[size];
+	yvals = new float[size];
+	trace.reserve(size);
+}
+
+void ChannelEvent::assign(const size_t &size_, const int &input_){
+	trace.assign(size_, input_);
+}
+
+void ChannelEvent::push_back(const int &input_){
+	trace.push_back(input_);
+}
+
+float ChannelEvent::CorrectBaseline(){
+	if(size == 0){ return -9999; }
+	else if(baseline_corrected){ return maximum; }
+
+	// Find the baseline
+	baseline = 0.0;
+	size_t sample_size = (10 <= size ? 10:size);
+	for(size_t i = 0; i < sample_size; i++){
+		baseline += (float)trace[i];
+	}
+	baseline = baseline/sample_size;
+	
+	// Calculate the standard deviation
+	stddev = 0.0;
+	for(size_t i = 0; i < sample_size; i++){
+		stddev += ((float)trace[i] - baseline)*((float)trace[i] - baseline);
+	}
+	stddev = std::sqrt((1.0/sample_size) * stddev);
+	
+	// Find the maximum value, the maximum bin, and correct the baseline
+	maximum = -9999.0;
+	for(size_t i = 0; i < trace.size(); i++){
+		xvals[i] = i;
+		yvals[i] = trace[i]-baseline;
+		if(yvals[i] > maximum){ 
+			maximum = yvals[i];
+			max_index = i;
+		}
+	}
+	
+	baseline_corrected = true;
+	
+	return maximum;
+}
+
+float ChannelEvent::FindLeadingEdge(const float &thresh_/*=0.05*/){
+	if(!baseline_corrected && CorrectBaseline() < 0){ return -9999; }
+	else if(phase != 0.0){ return phase; }
+
+	// Check if this is a valid pulse
+	if(maximum <= 0 || max_index == 0){ return -9999; }
+
+	for(size_t index = max_index; index > 0; index--){
+		if(yvals[index] <= thresh_ * maximum){ 
+			// Interpolate and return the value
+			// y = thresh_ * maximum
+			// x = (x1 + (y-y1)/(y2-y1))
+			// x1 = index, x2 = index+1
+			// y1 = yvals[index], y2 = yvals[index+1]
+			if(yvals[index+1] == yvals[index]){ return index+1; }
+			else{ return (phase = (index + (thresh_ * maximum - yvals[index])/(yvals[index+1] - yvals[index]))); }
+		}
+	}
+	
+	return -9999;
+}
+
+float ChannelEvent::FindQDC(const size_t &start_/*=0*/, const size_t &stop_/*=0*/){
+	if(!baseline_corrected && CorrectBaseline() < 0){ return -9999; }
+	else if(qdc != 0.0){ return qdc; }
+	
+	size_t stop = (stop_ == 0?size:stop_);
+	
+	qdc = 0.0;
+	for(size_t index = start_; index < stop; index++){
+		qdc += yvals[index];
+	}
+
+	return qdc;
+}
+
+void ChannelEvent::Clear(){
+	if(xvals){ delete[] xvals; }
+	if(yvals){ delete[] yvals; }
+	trace.clear();
+	size = 0;
+
+	energy = 0.0; 
+	time = 0.0;
+	
+	hires_energy = 0.0;
+	hires_time = 0.0;
+	valid_chan = false;
+	
+	for(int i = 0; i < numQdcs; i++){
+		qdcValue[i] = 0;
+	}
+
+	modNum = 0;
+	chanNum = 0;
+	trigTime = 0;
+	cfdTime = 0;
+	eventTimeLo = 0;
+	eventTimeHi = 0;
+
+	phase = 0.0;
+	maximum = 0.0;
+	baseline = 0.0;
+	stddev = 0.0;
+	qdc = 0.0;
+	max_index = 0;
+
+	virtualChannel = false;
+	pileupBit = false;
+	saturatedBit = false;
+	baseline_corrected = false;
+	ignore = false;
+}
