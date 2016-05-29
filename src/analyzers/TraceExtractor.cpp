24d5cff34a5700b133edb369bcc203c2829dbc67
S.V. Paulauskas <stanpaulauskas@gmail.com>
2016-05-29 14:05:33 -0400

    Fixing issue in TraceExtractor where it would not plot due to wrong Analze method being called.
    Signed-off-by: S.V. Paulauskas <stanpaulauskas@gmail.com>

--- src/analyzers/TraceExtractor.cpp
+++ src/analyzers/TraceExtractor.cpp
@@ -1,6 +1,7 @@
 /** \file TraceExtractor.cpp
  *  \brief Extract traces for a specific type and subtype
  */
+#include <iostream>
 #include <sstream>
 
 #include "Trace.hpp"
@@ -9,12 +10,15 @@
 
 using std::string;
 
+using namespace std;
+
 const unsigned int TraceExtractor::numTraces = dammIds::trace::extractor::maxSingleTraces;
 
 TraceExtractor::TraceExtractor(const std::string& aType,
-                               const std::string &aSubtype) :
-    type(aType), subtype(aSubtype) {
-    name = "Extractor";
+                               const std::string &aSubtype,
+			       const std::string &aTag) :
+    type(aType), subtype(aSubtype), tag(aTag){
+    name = "TraceExtractor";
 }
 
 void TraceExtractor::DeclarePlots(void)
@@ -26,12 +30,15 @@ void TraceExtractor::DeclarePlots(void)
 }
 
 void TraceExtractor::Analyze(Trace &trace, const std::string &aType,
-                             const std::string &aSubtype) {
+                             const std::string &aSubtype, 
+			     const std::map<std::string,int> &tags) {
     using namespace dammIds::trace::extractor;
     static unsigned int numPlottedTraces = 0;
 
-    if (type ==  aType && subtype == aSubtype && numPlottedTraces < numTraces){
-        TraceAnalyzer::Analyze(trace, type, subtype);
+    if (type ==  aType && subtype == aSubtype && 
+	(tag == "" || tags.find(tag) != tags.end()) &&
+	numPlottedTraces < numTraces){
+        TraceAnalyzer::Analyze(trace, type, subtype, tags);
         trace.OffsetPlot(D_TRACE, numPlottedTraces, 0.0);
         numPlottedTraces++;
         EndAnalyze(trace);
