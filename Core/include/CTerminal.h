1bc177fad26d965c3696a18ea3da3453cdd33a61
Cory R. Thornsberry <cthornsb@vols.utk.edu>
2016-06-13 22:17:43 -0400

    Resolved infamous CTerminal 'empty string' seg-fault
    Former-commit-id: b507ae4fe99aa67bac352e8a099e8373df5836f4

--- Core/include/CTerminal.h
+++ Core/include/CTerminal.h
@@ -25,7 +25,7 @@
 ///Default size of terminal scroll back buffer in lines.
 #define SCROLLBACK_SIZE 1000
 
-#define CTERMINAL_VERSION "1.2.06"
+#define CTERMINAL_VERSION "1.2.07"
 #define CTERMINAL_DATE "June 13th, 2016"
 
 #include <curses.h>
