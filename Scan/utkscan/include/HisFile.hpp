b58beca9f9cfc41d4fabfacd3a1f700851379e79
S.V. Paulauskas <stanpaulauskas@gmail.com>
2016-06-21 11:30:47 -0400

    Minor changes for test file to run

--- Scan/utkscan/include/HisFile.hpp
+++ Scan/utkscan/include/HisFile.hpp
@@ -109,29 +109,43 @@ struct drr_entry{
     drr_entry(){}
     
     /// Constructor for 1d histogram
-    drr_entry(unsigned int hisID_, unsigned short halfWords_, unsigned short raw_, unsigned short scaled_, unsigned short min_, unsigned short max_, const char * title_);
+    drr_entry(unsigned int hisID_, unsigned short halfWords_,
+              unsigned short raw_, unsigned short scaled_, unsigned short min_,
+              unsigned short max_, const char * title_);
     
     /// Constructor for 2d histogram
-    drr_entry(unsigned int hisID_, unsigned short halfWords_, unsigned short Xraw_, unsigned short Xscaled_, unsigned short Xmin_, unsigned short Xmax_,
-              unsigned short Yraw_, unsigned short Yscaled_, unsigned short Ymin_, unsigned short Ymax_, const char * title_);
+    drr_entry(unsigned int hisID_, unsigned short halfWords_, unsigned short Xraw_,
+              unsigned short Xscaled_, unsigned short Xmin_, unsigned short Xmax_,
+              unsigned short Yraw_, unsigned short Yscaled_, unsigned short Ymin_,
+              unsigned short Ymax_, const char * title_);
     
     /// Initialize variables not stored in the .drr entry
     void initialize();
     
     /// Check that a specified global bin is in range
-    bool check_bin(size_t bin_){ return ((bin_ < 0 || bin_ >= total_bins)?false:true); }
+    bool check_bin(size_t bin_){
+        return ((bin_ < 0 || bin_ >= total_bins)?false:true);
+    }
     
     /// Check that a specified x bin is in range.
-    bool check_x_bin(unsigned int x_){ return ((x_ < 0 || x_ > (unsigned int)scaled[0])?false:true); }
+    bool check_x_bin(unsigned int x_){
+        return ((x_ < 0 || x_ > (unsigned int)scaled[0])?false:true);
+    }
     
     /// Check that a specified x value is in range.
-    bool check_x_range(unsigned int x_){ return ((x_ < (unsigned int)minc[0] || x_ > (unsigned int)maxc[0])?false:true); }
+    bool check_x_range(unsigned int x_){
+        return ((x_ < (unsigned int)minc[0] || x_ > (unsigned int)maxc[0])?false:true);
+    }
     
     /// Check that a specified y bin is in range. Always returns true for 1d plots.
-    bool check_y_bin(unsigned int y_){ return (((y_ < 0 || y_ > (unsigned int)scaled[1]) && hisDim >= 2)?false:true); }
+    bool check_y_bin(unsigned int y_){
+        return (((y_ < 0 || y_ > (unsigned int)scaled[1]) && hisDim >= 2)?false:true);
+    }
     
     /// Check that a specified y value is in range. Always returns true for 1d plots.
-    bool check_y_range(unsigned int y_){ return (((y_ < (unsigned int)minc[1] || y_ > (unsigned int)maxc[1]) && hisDim >= 2)?false:true); }
+    bool check_y_range(unsigned int y_){
+        return (((y_ < (unsigned int)minc[1] || y_ > (unsigned int)maxc[1]) && hisDim >= 2)?false:true);
+    }
     
     /// Return the global array bin for a given x and y bin
     bool get_bin(unsigned int x_, unsigned int y_, unsigned int &bin);
@@ -159,8 +173,13 @@ struct fill_queue{
     bool good; /// True if the histo array index is within range
     
     fill_queue(drr_entry *entry_, unsigned int bin_, unsigned int w_){
-        entry = entry_; byte = bin_ * entry->halfWords * 2; weight = w_; good = entry->check_bin(bin_);
-        if(byte < 0){ std::cout << " his id = " << entry_->hisID << " byte offset is less than zero (" << byte << ")!\n"; }
+        entry = entry_;
+        byte = bin_ * entry->halfWords * 2;
+        weight = w_;
+        good = entry->check_bin(bin_);
+        if(byte < 0)
+            std::cout << " his id = " << entry_->hisID
+                      << " byte offset is less than zero (" << byte << ")!\n";
     }
 };
 
@@ -230,24 +249,30 @@ public:
     
     /// Get the number of bins in the y-axis
     short GetYbins();
-    
-    /*/// Get the number of bins in the z-axis
-      short GetZbins();
+
+    /*
+    /// Get the number of bins in the z-axis
+    short GetZbins();
       
-      /// Get the number of bins in the a-axis
-      short GetAbins();*/
+    /// Get the number of bins in the a-axis
+    short GetAbins();
+    */
     
     /// Get the range of a 1d histogram
     bool Get1dRange(short &xmin, short &xmax);
     
     /// Get the range of a 2d histogram
     bool Get2dRange(short &xmin, short &xmax, short &ymin, short &ymax);
-    
-    /*/// Get the range of a 3d histogram
-      bool Get3dRange(short &xmin, short &xmax, short &ymin, short &ymax, short &zmin, short &zmax);
+
+    /*
+    /// Get the range of a 3d histogram
+    bool Get3dRange(short &xmin, short &xmax, short &ymin, short &ymax, 
+                    short &zmin, short &zmax);
       
       /// Get the range of a 4d histogram
-      bool Get4dRange(short &xmin, short &xmax, short &ymin, short &ymax, short &zmin, short &zmax, short &amin, short &amax);*/
+    bool Get4dRange(short &xmin, short &xmax, short &ymin, short &ymax,
+                    short &zmin, short &zmax, short &amin, short &amax);
+    */
     
     /// Get the size of the .his file
     std::streampos GetHisFilesize();
