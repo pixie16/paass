b84f76ac0fdb0df4537c40b312919118ce2f9a28
S.V. Paulauskas <stanpaulauskas@gmail.com>
2016-08-08 07:14:19 -0400

    Address issues with relative paths
    Due to CLion's over zealousness some of the headers were changed to relative paths. This has been corrected. This commit closes #36 and #37
    
    Former-commit-id: 1832ca4a7bee257ec74633356eb3c45dc1d03d8a

--- Scan/ScanLib/source/Unpacker.cpp
+++ Scan/ScanLib/source/Unpacker.cpp
@@ -19,8 +19,8 @@
 #include <algorithm>
 #include <limits>
 
-#include "../include/Unpacker.hpp"
-#include "../include/XiaData.hpp"
+#include "Unpacker.hpp"
+#include "XiaData.hpp"
 
 void clearDeque(std::deque<XiaData*> &list){
 	while(!list.empty()){
