862781aa5d49a7a3aeeeee74820ca997378c8eef
Cory R. Thornsberry <cthornsb@vols.utk.edu>
2016-08-11 14:52:03 -0400

    Added variable init to all hribf buffers

--- Core/include/hribf_buffers.h
+++ Core/include/hribf_buffers.h
@@ -25,7 +25,7 @@
 #include <fstream>
 #include <vector>
 
-#define HRIBF_BUFFERS_VERSION "1.2.16"
+#define HRIBF_BUFFERS_VERSION "1.2.17"
 #define HRIBF_BUFFERS_DATE "Aug. 11th, 2016"
 
 #define ACTUAL_BUFF_SIZE 8194 /// HRIBF .ldf file format
