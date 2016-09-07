1ff29ac9691efac298cd0c7ef67d999290562c8d
Cory R. Thornsberry <cthornsb@vols.utk.edu>
2016-09-07 17:37:21 -0400

    Added cfd analyzer to scope program

--- Scan/ScanLib/source/XiaData.cpp
+++ Scan/ScanLib/source/XiaData.cpp
@@ -87,6 +87,7 @@ ChannelEvent::ChannelEvent(){
 	event = NULL;
 	xvals = NULL;
 	yvals = NULL;
+	cfdvals = NULL;
 	Clear();
 }
 
@@ -95,6 +96,7 @@ ChannelEvent::ChannelEvent(XiaData *event_){
 	event = NULL;
 	xvals = NULL;
 	yvals = NULL;
+	cfdvals = NULL;
 	Clear();
 	event = event_;
 	size = event->adcTrace.size();
@@ -188,12 +190,54 @@ float ChannelEvent::FindQDC(const size_t &start_/*=0*/, const size_t &stop_/*=0*
 	return qdc;
 }
 
+/// Perform CFD analysis on the waveform.
+float ChannelEvent::AnalyzeCFD(const float &F_/*=0.5*/, const size_t &D_/*=1*/, const size_t &L_/*=1*/){
+	if(!event || (!baseline_corrected && CorrectBaseline() < 0)){ return -9999; }
+	if(!cfdvals){
+		if(size == 0)
+			return -9999;
+		cfdvals = new float[size];
+	}
+	
+	float cfdMinimum = 9999;
+	size_t cfdMinIndex = 0;
+	
+	cfdCrossing = -9999;
+
+	// Compute the cfd waveform.
+	for(size_t cfdIndex = 0; cfdIndex < size; ++cfdIndex){
+		cfdvals[cfdIndex] = 0.0;
+		if(cfdIndex >= L_ + D_ - 1){
+			for(size_t i = 0; i < L_; i++)
+				cfdvals[cfdIndex] += F_ * yvals[cfdIndex - i] - yvals[cfdIndex - i - D_];
+		}
+		if(cfdvals[cfdIndex] < cfdMinimum){
+			cfdMinimum = cfdvals[cfdIndex];
+			cfdMinIndex = cfdIndex;
+		}
+	}
+
+	// Find the zero-crossing.
+	if(cfdMinIndex > 0){
+		// Find the zero-crossing.
+		for(size_t cfdIndex = cfdMinIndex-1; cfdIndex >= 0; cfdIndex--){
+			if(cfdvals[cfdIndex] >= 0.0 && cfdvals[cfdIndex+1] < 0.0){
+				cfdCrossing = xvals[cfdIndex] - cfdvals[cfdIndex]*(xvals[cfdIndex+1]-xvals[cfdIndex])/(cfdvals[cfdIndex+1]-cfdvals[cfdIndex]);
+				break;
+			}
+		}
+	}
+
+	return cfdCrossing;
+}
+
 void ChannelEvent::Clear(){
 	phase = -9999;
 	maximum = -9999;
 	baseline = -9999;
 	stddev = -9999;
 	qdc = -9999;
+	cfdCrossing = -9999;
 	max_index = 0;
 
 	hires_energy = -9999;
@@ -206,9 +250,11 @@ void ChannelEvent::Clear(){
 	size = 0;
 	if(xvals){ delete[] xvals; }
 	if(yvals){ delete[] yvals; }
+	if(cfdvals){ delete[] cfdvals; }
 	if(event){ event->clear(); }
 	
 	event = NULL;
 	xvals = NULL;
 	yvals = NULL;
+	cfdvals = NULL;
 }
