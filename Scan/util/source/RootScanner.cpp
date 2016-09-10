67b4e153d6723823965c9c4a08bf6608d91f48a8
Karl Smith <ksmit218@utk.edu>
2016-09-10 11:04:41 -0400

    Moved zoom info from scope to RootScanner.
    The RootScanner class can now track zoom information for a pad using the
    methods ResetZoom(TAxis*, TAxis*) and UpdateZoom(TAxis*, TAxis*). An
    optional argument of the pad can be provided with the default set to the
    current pad. The scope has been updated to use these methods.
    
    
    Former-commit-id: c177fca6d0ab6c3c3a36e8923548539159372cc5

--- Scan/util/source/RootScanner.cpp
+++ Scan/util/source/RootScanner.cpp
@@ -2,11 +2,12 @@
 
 #include "TSystem.h"
 
-RootScanner::RootScanner() : ScanInterface() {
+RootScanner::RootScanner() : 
+	ScanInterface()
+{
 	new TApplication("scanner", 0, NULL);
 
 	canvas_ = new TCanvas("canvas", "");
-	
 }
 RootScanner::~RootScanner() {
 	canvas_->Close();
@@ -22,3 +23,68 @@ void RootScanner::IdleTask() {
 	gSystem->ProcessEvents();
 	usleep(100000);
 }
+
+void RootScanner::ResetZoom(TVirtualPad *pad /*= gPad*/) {
+	AxisInfo* padZoomInfo = &zoomInfo_[pad];
+	for (int i=0;i<2;i++) {
+		padZoomInfo->axisVals[i][0] = 1E9;
+		padZoomInfo->axisVals[i][1] = -1E9;
+		padZoomInfo->userZoomVals[i][0] = 1E9;
+		padZoomInfo->userZoomVals[i][1] = -1E9;
+	}
+	padZoomInfo->reset = true;
+}
+
+/**The following routine permits a plot to auto zoom unless the user has set 
+ * the zoom. If the user has zoomed the axis extents are stored, but the user
+ * zoom is set.
+ */
+void RootScanner::UpdateZoom(TAxis *axisX, TAxis *axisY, TVirtualPad *pad /*=gPad*/) {
+	AxisInfo* padZoomInfo = &zoomInfo_[pad];
+
+	//If zoom has been reset we continue, otherwise we get current axis limits.
+	if (padZoomInfo->reset) padZoomInfo->reset = false;
+	else {
+		//Get the user zoom settings.
+		padZoomInfo->userZoomVals[0][0] = canvas_->GetUxmin();
+		padZoomInfo->userZoomVals[0][1] = canvas_->GetUxmax();
+		padZoomInfo->userZoomVals[1][0] = canvas_->GetUymin();
+		padZoomInfo->userZoomVals[1][1] = canvas_->GetUymax();
+	}
+
+	//Determine if the user had zoomed or unzoomed by comparing the current axis
+	// limits to those taken from the canvas.
+	bool userZoom[2];
+	for (int i=0; i<2; i++) {
+		userZoom[i] =  (padZoomInfo->userZoomVals[i][0] != padZoomInfo->axisVals[i][0] || 
+			padZoomInfo->userZoomVals[i][1] != padZoomInfo->axisVals[i][1]);
+	}
+
+	//If the axis min / max are outside current stored values thens We update 
+	// the values.
+	if (axisX->GetXmin() < padZoomInfo->axisVals[0][0]) 
+		padZoomInfo->axisVals[0][0] = axisX->GetXmin(); 
+	if (axisX->GetXmax() > padZoomInfo->axisVals[0][1]) 
+		padZoomInfo->axisVals[0][1] = axisX->GetXmax(); 
+	if (axisY->GetXmin() < padZoomInfo->axisVals[1][0]) 
+		padZoomInfo->axisVals[1][0] = axisY->GetXmin(); 
+	if (axisY->GetXmax() > padZoomInfo->axisVals[1][1]) 
+		padZoomInfo->axisVals[1][1] = axisY->GetXmax(); 
+	//Set the limits of the axis to the determined min / max.
+	axisX->SetLimits(padZoomInfo->axisVals[0][0], padZoomInfo->axisVals[0][1]);
+	axisY->SetLimits(padZoomInfo->axisVals[1][0], padZoomInfo->axisVals[1][1]);
+
+	//If the user didn't zoom we store the current axis limits in the userZoom 
+	// values.
+	for (int i = 0; i < 2; i++) {
+		if (!userZoom[i]) {
+			for (int j = 0; j < 2; j++) padZoomInfo->userZoomVals[i][j] = padZoomInfo->axisVals[i][j];
+		}
+	}
+
+	//Set the range of the axis to the determined userZoom values.
+	axisX->SetRangeUser(padZoomInfo->userZoomVals[0][0], padZoomInfo->userZoomVals[0][1]);
+	axisY->SetRangeUser(padZoomInfo->userZoomVals[1][0], padZoomInfo->userZoomVals[1][1]);
+
+}
+
