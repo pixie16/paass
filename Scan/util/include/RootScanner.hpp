c0fcb107ab482e1e97f0288b52d01f896941133a
Karl Smith <ksmit218@utk.edu>
2016-09-10 11:04:41 -0400

    Moved zoom info from scope to RootScanner.
    The RootScanner class can now track zoom information for a pad using the
    methods ResetZoom(TAxis*, TAxis*) and UpdateZoom(TAxis*, TAxis*). An
    optional argument of the pad can be provided with the default set to the
    current pad. The scope has been updated to use these methods.
    
    
    Former-commit-id: c177fca6d0ab6c3c3a36e8923548539159372cc5

--- Scan/util/include/RootScanner.hpp
+++ Scan/util/include/RootScanner.hpp
@@ -1,10 +1,13 @@
 #ifndef ROOTSCANNER_H
 #define ROOTSCANNER_H
 
-#include "ScanInterface.hpp"
+#include <map>
 
 #include "TCanvas.h"
 #include "TApplication.h"
+#include "TAxis.h"
+
+#include "ScanInterface.hpp"
 
 class RootScanner : public ScanInterface {
 	public:
@@ -12,9 +15,18 @@ class RootScanner : public ScanInterface {
 		~RootScanner();
 		TCanvas *GetCanvas() {return canvas_;};
 		void IdleTask();
+		void UpdateZoom(TAxis *axisX, TAxis *axisY, TVirtualPad* pad = gPad);
+		void ResetZoom(TVirtualPad *pad = gPad);
 	
 	private:
 		TCanvas *canvas_;
+	
+		struct AxisInfo {	
+			float userZoomVals[2][2];
+			float axisVals[2][2];
+			bool reset;
+		};
+		std::map< TVirtualPad*, AxisInfo > zoomInfo_;
 
 };
 
