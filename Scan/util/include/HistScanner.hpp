ca876e5c2bc0f8d97ee21d9b2a56248442823f0f
Karl Smith <ksmit218@utk.edu>
2016-09-29 16:53:07 -0400

    Updated RootScanner::UpdateZoom.
    The UpdateZoom now only takes an argument for a TVirtualPad* and scans
    the items in that pad for TGraph and TH1 objects. These are then used to
    compute the pad limits. The zoom controls are handled in a similar way.
    
    This updated caused a minor change in the call from scope.
    
    This update permitted the autozooming the work in a fashion similar to
    the expectation for HistScanner. In addition, it permitted a substantial
    reduction int he code overhead in HistScanner.
    
    
    Former-commit-id: abfea8a3a430a2beee85d307b197e70ca0dcb938

--- Scan/util/include/HistScanner.hpp
+++ Scan/util/include/HistScanner.hpp
@@ -13,9 +13,11 @@
 
 class HistScanner : public RootScanner {
 	public:
+		/// @brief Default constructor.
 		HistScanner();
-		/// Destructor.
+		/// @brief Default destructor.
 		virtual ~HistScanner();
+		/// @brief Processes each event by filling the tree and clearing the event.
 		bool ProcessEvents();
 		/** ExtraCommands is used to send command strings to classes derived
 		 * from ScanInterface. If ScanInterface receives an unrecognized
@@ -29,27 +31,42 @@ class HistScanner : public RootScanner {
 
 
 	protected:
+		/// @brief Create a HistScannerChanData from the porivded XiaData.
+		/// @param[in] event An XIA event to process.
 		bool AddEvent(XiaData* event);
 
 	private:
-		TFile *file_;
-		TTree *tree_; //<Tree containing the current plottable data.
 		Unpacker* GetCore();
 
-		std::vector< HistScannerChanData > *eventData_; //< Vector containing all the channel data for an event.
+		TFile *file_; //<ROOT file containing the tree.
+		TTree *tree_; //<Tree containing the current plottable data.
 
+		/// Vector containing all the channel data for an event.
+		std::vector< HistScannerChanData > *eventData_; 
+
+		/// The type for key for the hist map.
 		typedef std::tuple< int, int, std::string > HistKey_;
-		std::vector< std::pair< HistKey_, TVirtualPad* > > newHists_;
+		/// The type for the histogram map.
 		typedef std::map< HistKey_, std::string > HistMap_;
+
+		///A vector of new histograms, containing a HistKey_ and a TVirtualPad*.
+		std::vector< std::pair< HistKey_, TVirtualPad* > > newHists_;
+		///A map of plotted histograms.
 		std::map< TVirtualPad*, HistMap_ > histos_;
+		///A map whose value is the number of times a histogram key was requested
+		/// for plotting.
 		std::map< HistKey_, int > histCount_;
+		///The number of TTree entries that had been scanned to create the 
+		/// corresponding histogram.
+		std::map< TH1*, long > treeEntries_;
 
-		std::map< TVirtualPad*, std::pair< float, float > > padMaximums_;
-
+		///@brief Initialize and plot newly requested plots.
 		void ProcessNewHists();
+		///@brief Plot the histogram corresponding to the provided key.
+		///@param[in] key The key of the histogram to plot.
+		///@param[in] par The corresponding pad that the histogram should be 
+		/// plotted on
 		void Plot(HistKey_ key, TVirtualPad *pad = gPad);
-	
-		void ScaleHistograms(TVirtualPad* pad);
 
 };
 
--- Scan/util/source/RootScanner.cpp
+++ Scan/util/source/RootScanner.cpp
@@ -1,6 +1,13 @@
 #include "RootScanner.hpp"
 
+#include <limits>
+
 #include "TSystem.h"
+#include "TList.h"
+#include "TObject.h"
+#include "TAxis.h"
+#include "TH1.h"
+#include "TGraph.h"
 
 RootScanner::RootScanner() : 
 	ScanInterface()
@@ -28,19 +35,17 @@ void RootScanner::IdleTask() {
 void RootScanner::ResetZoom(TVirtualPad *pad /*= gPad*/) {
 	AxisInfo* padZoomInfo = &zoomInfo_[pad];
 	for (int i=0;i<2;i++) {
-		padZoomInfo->axisVals[i][0] = 1E9;
-		padZoomInfo->axisVals[i][1] = -1E9;
-		padZoomInfo->userZoomVals[i][0] = 1E9;
-		padZoomInfo->userZoomVals[i][1] = -1E9;
+		padZoomInfo->limitMin[i] = std::numeric_limits<float>::max();
+		padZoomInfo->limitMax[i] = std::numeric_limits<float>::min();
+		padZoomInfo->rangeUserMin[i] = std::numeric_limits<float>::max();
+		padZoomInfo->rangeUserMax[i] = std::numeric_limits<float>::min();
 	}
 	padZoomInfo->reset = true;
 }
 
-/**The following routine permits a plot to auto zoom unless the user has set 
- * the zoom. If the user has zoomed the axis extents are stored, but the user
- * zoom is set.
- */
-void RootScanner::UpdateZoom(TAxis *axisX, TAxis *axisY, TVirtualPad *pad /*=gPad*/) {
+//Update the zoom levels on a pad.
+void RootScanner::UpdateZoom(TVirtualPad *pad /*= gPad*/) {
+	//Get zoom info fo this pad.
 	auto itr = zoomInfo_.find(pad);
 	if (itr == zoomInfo_.end()) ResetZoom(pad);
 	AxisInfo* padZoomInfo = &zoomInfo_[pad];
@@ -49,46 +54,93 @@ void RootScanner::UpdateZoom(TAxis *axisX, TAxis *axisY, TVirtualPad *pad /*=gPa
 	if (padZoomInfo->reset) padZoomInfo->reset = false;
 	else {
 		//Get the user zoom settings.
-		padZoomInfo->userZoomVals[0][0] = pad->GetUxmin();
-		padZoomInfo->userZoomVals[0][1] = pad->GetUxmax();
-		padZoomInfo->userZoomVals[1][0] = pad->GetUymin();
-		padZoomInfo->userZoomVals[1][1] = pad->GetUymax();
+		padZoomInfo->rangeUserMin[0] = pad->GetUxmin();
+		padZoomInfo->rangeUserMax[0] = pad->GetUxmax();
+		padZoomInfo->rangeUserMin[1] = pad->GetUymin();
+		padZoomInfo->rangeUserMax[1] = pad->GetUymax();
 	}
 
 	//Determine if the user had zoomed or unzoomed by comparing the current axis
 	// limits to those taken from the canvas.
 	bool userZoom[2];
 	for (int i=0; i<2; i++) {
-		userZoom[i] =  (padZoomInfo->userZoomVals[i][0] > padZoomInfo->axisVals[i][0] || 
-			padZoomInfo->userZoomVals[i][1] < padZoomInfo->axisVals[i][1]);
+		userZoom[i] =  (padZoomInfo->rangeUserMin[i] > padZoomInfo->limitMin[i] || 
+			padZoomInfo->rangeUserMax[i] < padZoomInfo->limitMax[i]);
+	}	
+
+	//Get the list of items on the pad.
+	TList *list = gPad->GetListOfPrimitives();
+
+	//Loop over the objects in the list to determine pad limits.
+	for( TObject *obj = list->First(); obj; obj = list->After(obj)) {
+		TAxis *xAxis, *yAxis; //Pointers to the axes.
+		//Check if the object is a histogram
+		if ( TH1* hist = dynamic_cast<TH1*>(obj) ) {
+			xAxis = hist->GetXaxis();
+			yAxis = hist->GetYaxis();
+			if (hist->GetBinContent(hist->GetMaximumBin()) > padZoomInfo->limitMax[1]) {
+				padZoomInfo->limitMax[1] = 1.1 * hist->GetBinContent(hist->GetMaximumBin());
+			}
+		}
+		//Check if the object is a graph
+		else if ( TGraph* graph = dynamic_cast<TGraph*>(obj) ) {
+			xAxis = graph->GetXaxis();
+			yAxis = graph->GetYaxis();
+		}
+		//Not an object we care about so we continue.
+		else continue;
+
+		//If the axis min / max are outside current stored values thens We update 
+		// the values.
+		if (xAxis->GetXmin() < padZoomInfo->limitMin[0]) 
+			padZoomInfo->limitMin[0] = xAxis->GetXmin(); 
+		if (xAxis->GetXmax() > padZoomInfo->limitMax[0]) 
+			padZoomInfo->limitMax[0] = xAxis->GetXmax(); 
+		if (yAxis->GetXmin() < padZoomInfo->limitMin[1]) 
+			padZoomInfo->limitMin[1] = yAxis->GetXmin(); 
+		if (yAxis->GetXmax() > padZoomInfo->limitMax[1]) 
+			padZoomInfo->limitMax[1] = yAxis->GetXmax(); 
+
 	}
-
-	//If the axis min / max are outside current stored values thens We update 
-	// the values.
-	if (axisX->GetXmin() < padZoomInfo->axisVals[0][0]) 
-		padZoomInfo->axisVals[0][0] = axisX->GetXmin(); 
-	if (axisX->GetXmax() > padZoomInfo->axisVals[0][1]) 
-		padZoomInfo->axisVals[0][1] = axisX->GetXmax(); 
-	if (axisY->GetXmin() < padZoomInfo->axisVals[1][0]) 
-		padZoomInfo->axisVals[1][0] = axisY->GetXmin(); 
-	if (axisY->GetXmax() > padZoomInfo->axisVals[1][1]) 
-		padZoomInfo->axisVals[1][1] = axisY->GetXmax(); 
-
-	//Set the limits of the axis to the determined min / max.
-	axisX->SetLimits(padZoomInfo->axisVals[0][0], padZoomInfo->axisVals[0][1]);
-	axisY->SetLimits(padZoomInfo->axisVals[1][0], padZoomInfo->axisVals[1][1]);
 
 	//If the user didn't zoom we store the current axis limits in the userZoom 
 	// values.
-	for (int i = 0; i < 2; i++) {
-		if (!userZoom[i]) {
-			for (int j = 0; j < 2; j++) padZoomInfo->userZoomVals[i][j] = padZoomInfo->axisVals[i][j];
+	for (int axis = 0; axis < 2; axis++) {
+		if (!userZoom[axis]) {
+			padZoomInfo->rangeUserMin[axis] = padZoomInfo->limitMin[axis];
+			padZoomInfo->rangeUserMax[axis] = padZoomInfo->limitMax[axis];
 		}
 	}
 
-	//Set the range of the axis to the determined userZoom values.
-	axisX->SetRangeUser(padZoomInfo->userZoomVals[0][0], padZoomInfo->userZoomVals[0][1]);
-	axisY->SetRangeUser(padZoomInfo->userZoomVals[1][0], padZoomInfo->userZoomVals[1][1]);
+	//Loop over the objects again and set the proper limits for each item.
+	for( TObject *obj = list->First(); obj; obj = list->After(obj)) {
+		TAxis *xAxis, *yAxis; //Pointers to the axes.
+		//Check if the object is a histogram
+		if ( TH1* hist = dynamic_cast<TH1*>(obj) ) {
+			xAxis = hist->GetXaxis();
+			yAxis = hist->GetYaxis();
+
+			//Set the axes limits
+			xAxis->SetLimits(padZoomInfo->limitMin[0], padZoomInfo->limitMax[0]);
+			yAxis->SetLimits(padZoomInfo->limitMin[1], padZoomInfo->limitMax[1]);
+			//Set the histogram maximum
+			hist->SetMaximum(padZoomInfo->limitMax[1]);	
+
+		}
+		//Check if the object is a graph
+		else if ( TGraph* graph = dynamic_cast<TGraph*>(obj) ) {
+			xAxis = graph->GetXaxis();
+			yAxis = graph->GetYaxis();
+		}
+		else continue;
+
+		//Set the range of the axis to the determined userZoom values.
+		xAxis->SetRangeUser(padZoomInfo->rangeUserMin[0], padZoomInfo->rangeUserMax[0]);
+		yAxis->SetRangeUser(padZoomInfo->rangeUserMin[1], padZoomInfo->rangeUserMax[1]);
+	}
+		
+	pad->Modified();
+	canvas_->Update();
 
 }
 
