54ca80d5408405155a09034a5f731d6b6922da30
S.V. Paulauskas <stanpaulauskas@gmail.com>
2016-11-25 14:54:46 -0500

    Fixed up compilation so that we can build the unit tests for Core
    The CMakeLists.txt have all been updated so that we can begin to build the tests for the classes in Core. I have added the UnitTest++ package as a required package before the utkscan tests can be built. 
    
    Former-commit-id: 616328208e82f476620a24d32009326bb04541c8

---
+++ Scan/utkscan/core/tests/unittest-WalkCorrector.cpp
@@ -0,0 +1,9 @@
+///@file unittest-WalkCorrector.cpp
+///@brief Main driver to test the Walk Corrector.
+///@author S. V. Paulauskas
+///@date November 25, 2016
+#include <UnitTest++.h>
+
+int main(int argv, char* argc[]) {
+    return(UnitTest::RunAllTests());
+}
