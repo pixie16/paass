5c6e9bbb5daa02509f1f9f67027483ca67cda337
S.V. Paulauskas <stanpaulauskas@gmail.com>
2016-12-20 18:33:53 -0500

    Implementing TraditionalCfd
    This CFD performs like a traditional CFD does. It applies a delay to the signal, inverts it and then subtracts it. It uses a linear interpolation between the max and min to find the zero crossing. it works just as well as expected. 
    
    Former-commit-id: f7cadc503432d17d7d9be68d300639d145ee91a9

---
+++ Scan/Resources/include/TraditionalCfd.hpp
@@ -0,0 +1,24 @@
+///@file TraditionalCfd.hpp
+///@brief Traditional CFD implemented digitally
+///@author S. V. Paulauskas
+///@date July 22, 2011
+
+#ifndef PIXIESUITE_TRADITIONALCFD_HPP
+#define PIXIESUITE_TRADITIONALCFD_HPP
+
+#include "TimingDriver.hpp"
+
+class TraditionalCfd : public TimingDriver {
+public:
+    /// Default constructor
+    TraditionalCfd() {};
+
+    /// Default destructor
+    ~TraditionalCfd() {};
+
+    double CalculatePhase(const std::vector<unsigned int> &data,
+                          const std::pair<double, double> &pars,
+                          const std::pair<unsigned int, double> &max,
+                          const std::pair<double, double> baseline);
+};
+#endif //PIXIESUITE_TRADITIONALCFD_HPP
