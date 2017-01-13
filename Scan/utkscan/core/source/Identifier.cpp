eff406f186550eec42cfc5d2892ecb622ff730f2
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-01-13 10:59:36 -0500

    Fixing issue #168

--- Scan/utkscan/core/source/Identifier.cpp
+++ Scan/utkscan/core/source/Identifier.cpp
@@ -9,22 +9,20 @@
 
 using namespace std;
 
-
-
 int Identifier::GetTag(const std::string &s) const {
-    map<string, int>::const_iterator it = tag.find(s);
-    if (it == tag.end())
+    map<string, int>::const_iterator it = tag_.find(s);
+    if (it == tag_.end())
         return(std::numeric_limits<int>::max());
     return it->second;
 }
 
 void Identifier::Zero() {
-    dammID   = -1;
-    location = -1;
-    type     = "";
-    subtype  = "";
+    dammID_   = 9999;
+    location_ = 9999;
+    type_     = "";
+    subtype_  = "";
 
-    tag.clear();
+    tag_.clear();
 }
 
 void Identifier::PrintHeaders(void) {
@@ -36,14 +34,14 @@ void Identifier::PrintHeaders(void) {
 }
 
 void Identifier::Print(void) const {
-    cout << setw(10) << type
-	 << setw(10) << subtype
-	 << setw(4)  << location
-	 << setw(6)  << dammID
+    cout << setw(10) << type_
+	 << setw(10) << subtype_
+	 << setw(4)  << location_
+	 << setw(6)  << dammID_
 	 << "    ";
-    for (map<string, int>::const_iterator it = tag.begin();
-	 it != tag.end(); it++) {
-	if (it != tag.begin())
+    for (map<string, int>::const_iterator it = tag_.begin();
+	 it != tag_.end(); it++) {
+	if (it != tag_.begin())
 	    cout << ", ";
 	cout << it->first << "=" << it->second;
     }
