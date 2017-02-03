bbccb7e6ecba5dde6cad3afe2f0e4ad370d881d4
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-02-03 16:45:24 -0500

    Updating the Analyzers so that they now plot correctly.

--- Analysis/Utkscan/analyzers/source/TraceFilterAnalyzer.cpp
+++ Analysis/Utkscan/analyzers/source/TraceFilterAnalyzer.cpp
@@ -27,12 +27,12 @@ void TraceFilterAnalyzer::DeclarePlots(void) {
     const int traceBins = dammIds::trace::traceBins;
     const unsigned short numTraces = Globals::get()->numTraces();
 
-    sample_trace.DeclareHistogram1D(D_RETVALS, S3,"Retvals for Filtering");
-    sample_trace.DeclareHistogram2D(DD_TRIGGER_FILTER, traceBins, numTraces,
+    DeclareHistogram1D(D_RETVALS, S3,"Retvals for Filtering");
+    DeclareHistogram2D(DD_TRIGGER_FILTER, traceBins, numTraces,
                                     "Trigger Filter");
-    sample_trace.DeclareHistogram2D(DD_REJECTED_TRACE, traceBins, numTraces,
+    DeclareHistogram2D(DD_REJECTED_TRACE, traceBins, numTraces,
                                     "Rejected Traces");
-    sample_trace.DeclareHistogram2D(DD_PILEUP, traceBins, numTraces,
+    DeclareHistogram2D(DD_PILEUP, traceBins, numTraces,
                                     "Rejected Traces");
 }
 
@@ -56,42 +56,25 @@ void TraceFilterAnalyzer::Analyze(Trace &trace, const std::string &type,
     
     //if retval != 0 there was a problem and we should look at the trace
     if(retval != 0) {
-	trace.Plot(D_RETVALS,retval);
+	plot(D_RETVALS, retval);
 	if (numRejected < numTraces)
-            trace.Plot(DD_REJECTED_TRACE, numRejected++);
+            plot(DD_REJECTED_TRACE, numRejected++);
     }
-    
-    vector<double> tfilt = filter.GetTriggerFilter();
-    trace.SetTriggerFilter(tfilt);
-    trace.SetValue("numTriggers", (int)filter.GetNumTriggers());
+
+    trace.SetTriggerFilter(filter.GetTriggerFilter());
+    trace.SetTriggerPositions(filter.GetTriggers());
+    trace.SetFilteredEnergies(filter.GetEnergies());
+    trace.SetEnergySums(filter.GetEnergySums());
+    trace.SetFilteredBaseline(filter.GetBaseline());
 
     //plot traces that were flagged as pileups
     if(filter.GetHasPileup() && numPileup < numTraces)
-	trace.Plot(DD_PILEUP,numPileup++);
+        plot(DD_PILEUP,numPileup++);
 
-    //We will record in the trace all triggers that were found. 
-    vector<unsigned int> trigs = filter.GetTriggers();
-    stringstream ss;
-    for(unsigned int i = 0; i < trigs.size(); i++) {
-	ss << "triggerPosition" << i;
-	trace.SetValue(ss.str(), (int)trigs[i]);
-	ss.str("");
-    }
-
-    //We will record all the energies that were recorded
-    vector<double> energies = filter.GetEnergies();
-    for(unsigned int i = 0; i < trigs.size(); i++) {
-	ss << "filterEnergy" << i;
-	trace.SetValue(ss.str(), (int)trigs[i]);
-	ss.str("");
-    }
-    
-    trace.SetValue("baseline", filter.GetBaseline());
-    trace.SetEnergySums(filter.GetEnergySums());
-    
     //500 is an arbitrary offset since DAMM cannot display negative numbers.
+    vector<double> tfilt = filter.GetTriggerFilter();
     for(vector<double>::iterator it = tfilt.begin(); it != tfilt.end(); it++)
-	trace.plot(DD_TRIGGER_FILTER, (int)(it-tfilt.begin()),
+	plot(DD_TRIGGER_FILTER, (int)(it-tfilt.begin()),
 		   numTrigFilters, (*it)+500);
     numTrigFilters++;
    
