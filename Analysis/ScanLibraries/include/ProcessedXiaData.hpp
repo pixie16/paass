d440e3904ed1f1bd66a851613af625ceca1b4285
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-02-03 13:49:33 -0500

    ProcessedXiaData now contains the Trace class. Adding Methods.
    The Trace class was added here since it's the logical place for it to be carried around. This also mimics what was previously done in utkscan::ChanEvent, and the ChannelEvent class. 
    
    I've also added some new methods to the class for walk corrections, energy calibrations, etc. These are data that have been massaged in some way.

--- Analysis/ScanLibraries/include/ProcessedXiaData.hpp
+++ Analysis/ScanLibraries/include/ProcessedXiaData.hpp
@@ -4,8 +4,8 @@
 ///@date December 2, 2016
 #ifndef PIXIESUITE_PROCESSEDXIADATA_HPP
 #define PIXIESUITE_PROCESSEDXIADATA_HPP
-#include <memory>
 
+#include "Trace.hpp"
 #include "XiaData.hpp"
 
 ///This class contains additional information about the XiaData after
@@ -15,25 +15,61 @@
 class ProcessedXiaData : public XiaData {
 public:
     /// Default constructor.
-    ProcessedXiaData() {Clear();}
+    ProcessedXiaData() {}
 
     /// Constructor from XiaData. ProcessedXiaData will take ownership via
-    /// the unique pointer.
+    /// the unique pointer. We also assign the trace to the Trace class and
+    /// set the saturation flag in the Trace. This will allow people to
+    /// access this information should the Trace be passed separately from
+    /// the rest of the data.
     ProcessedXiaData(XiaData *event) {
         data_ = event;
+        trace_ = data_->GetTrace();
+        trace_.SetIsSaturated(data_->IsSaturated());
     }
 
     /// Default Destructor.
-    ~ProcessedXiaData(){};
+    ~ProcessedXiaData() {};
+
+    ///\return The calibrated energy for the channel
+    double GetCalibratedEnergy() const { return calibratedEnergy_; }
+
+    ///\return The Walk corrected time of the channel
+    double GetWalkCorrectedTime() const { return walkCorrectedTime_; }
+
+    ///\return The sub-sampling arrival time of the signal in nanoseconds.
+    double GetHighResTimeInNs() const { return highResTimeInNs_; }
+
+    ///Set the calibrated energy
+    ///@param [in] a : the calibrated energy
+    void SetCalibratedEnergy(const double &a) { calibratedEnergy_ = a; }
+
+    ///Set to True if we would like to ignore this channel
+    ///@param[in] a : The value that we want to set
+    void SetIsIgnored(const bool &a) { isIgnored_ = a; }
+
+    ///Set to true if the energy and time are not bogus values.
+    ///@param[in] a : The value that we would like to set
+    void SetIsValidData(const bool &a) { isValidData_; }
+
+    ///Set the high resolution time (Filter time (sans CFD) + phase )
+    ///@param [in] a : the high resolution time
+    void SetHighResTime(const double &a) { highResTimeInNs_ = a; }
+
+    ///Set the Walk corrected time
+    ///@param [in] a : the walk corrected time */
+    void SetWalkCorrectedTime(const double &a) { walkCorrectedTime_ = a; }
+
 private:
-    ///A pointer to the XiaData
-    XiaData *data_;
+    XiaData *data_; ///< A pointer to the XiaData that we now own.
+    Trace trace_; ///< A Trace object to handle the Trace related stuff.
 
-    /// Clear all variables and clear the trace vector and arrays.
-    void Initialize(){};
+    bool isIgnored_; ///< True if we ignore this event.
+    bool isValidData_; ///< True if the energy and High Res time are valid.
 
-    bool isIgnored_; /// Ignore this event.
-    bool isValidData; /// True if the high resolution energy and time are valid.
-    double hires_time; /// High resolution time taken from pulse fits (in ns).
+    double calibratedEnergy_; ///< The energy after calibration
+    double highResTimeInNs_; ///< High Res time taken from pulse fits (in ns).
+    double walkCorrectedTime_; ///< The time after walk corrections
 };
+
 #endif //PIXIESUITE_PROCESSEDXIADATA_HPP
