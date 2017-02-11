a06ba904db5c9fe19e0ad4ea20de2f87bd48c45b
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-02-11 11:02:32 -0500

    Adding a header that contains various constants.
    These constants are things that users will likely never need to modify. For example, the length of a VANDLE bar in cm or the number of channels in a Pixie-16 module.

--- Resources/include/Constants.hpp
+++ Resources/include/Constants.hpp
@@ -6,9 +6,26 @@
 #ifndef PIXIESUITE_CONSTANTS_HPP
 #define PIXIESUITE_CONSTANTS_HPP
 
+///Namespace containing information about Pixie16 modules.
 namespace Pixie16 {
+    ///This is the maximum number of channels that a Pixie-16 module can have.
     static const unsigned int maximumNumberOfChannels = 16;
 
+}
+
+///Namespace containing physics constants that can be used during the analysis.
+namespace Physical {
+    static const double neutronMassInMeVcc = 939.565560;
+    static const double speedOfLightInCmPerNs = 29.9792458;
+}
+
+///Namespace containing information relevant for VANDLE detectors. These
+/// values are generally values that the users should not be editing, i.e.
+/// the length of a medium bar does not change.
+namespace Vandle {
+    static const double lengthOfSmallBarInCm = 60;
+    static const double lengthOfMediumBarInCm = 120;
+    static const double lengthOfBigBarInCm = 200;
 }
 
 #endif //PIXIESUITE_CONSTANTS_HPP
