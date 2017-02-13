bcf8de77260af8d70f689ee6c533dc99dbccb2f6
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-02-13 09:13:00 -0500

    Abstracting the XmlParser and moving to Resources
    This class actually has some use to other programs and classes. I have abstracted the class and moved it to Resources so that it can be used else where. This allows us to define error checking methods for all of the XML based readers.

---
+++ Resources/include/XmlParser.hpp
@@ -0,0 +1,38 @@
+///@file XmlParser.hpp
+///@brief Class to help parse nodes XML based files.
+///@author S. V. Paulauskas
+///@date February 09, 2017
+#ifndef _PAASS_XMLPARSER_HPP
+#define _PAASS_XMLPARSER_HPP
+#include <set>
+
+#include "pugixml.hpp"
+
+///A class that handles parsing XML nodes
+class XmlParser {
+public:
+    ///Default Constructor
+    XmlParser() {}
+
+    ///Default Destructor
+    virtual ~XmlParser() {}
+
+    ///Parses the provided node from an opened xml file.
+    ///@param[in] node : The node that we are going to parse
+    ///@throw invalid_argument if the node cannot be found.
+    virtual void ParseNode(const pugi::xml_node &node);
+
+    ///Warn that we have an unknown parameter in the node.
+    ///@param [in] node : an iterator pointing to the location of the unknown
+    ///@param [in] knownChildren: A list of the nodes that are known.
+    virtual void WarnOfUnknownChildren(
+            const pugi::xml_node &node,
+            const std::set<std::string> &knownChildren);
+private:
+    ///Constructs a message string for the throws
+    ///@param[in] name : The name of the node where we had the error
+    ///@return The message that we want the throw to contain.
+    std::string CriticalNodeMessage(const std::string &name);
+};
+
+#endif //#ifdef _PAASS_XMLPARSER_HPP
