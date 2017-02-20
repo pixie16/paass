2133c399663e878da16ef2e9a18508af20fbca5c
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-02-20 18:11:28 -0500

    XML parsing removed from TreeCorrelator and into TreeCorrelatorXmlParser
    This parser handles parsing the TreeCorrelator node and adding the information to the TreeCorrelator. This  may require some cleanup later but it is working as expected.

--- Analysis/Utkscan/core/source/TreeCorrelator.cpp
+++ Analysis/Utkscan/core/source/TreeCorrelator.cpp
@@ -10,33 +10,10 @@
 #include "Messenger.hpp"
 #include "StringManipulationFunctions.hpp"
 #include "TreeCorrelator.hpp"
+#include "TreeCorrelatorXmlParser.hpp"
 
 using namespace std;
 using namespace StringManipulation;
-
-void Walker::parsePlace(pugi::xml_node node, std::string parent, bool verbose) {
-    map<string, string> params;
-    params["parent"] = parent;
-    params["type"] = "";
-    params["reset"] = "true";
-    params["coincidence"] = "true";
-    params["fifo"] = "2";
-    params["init"] = "false";
-    for (pugi::xml_attribute attr = node.first_attribute(); attr;
-         attr = attr.next_attribute()) {
-        params[attr.name()] = attr.value();
-    }
-    TreeCorrelator::get()->createPlace(params, verbose);
-}
-
-void Walker::traverseTree(pugi::xml_node node, std::string parent, bool verbose) {
-    for (pugi::xml_node child = node.child("Place");
-         child;
-         child = child.next_sibling("Place")) {
-        parsePlace(child, parent, verbose);
-        traverseTree(child, string(child.attribute("name").value()), verbose);
-    }
-}
 
 TreeCorrelator* TreeCorrelator::instance = NULL;
 
@@ -77,45 +54,6 @@ void TreeCorrelator::addChild(std::string parent, std::string child,
            << " as a child of " << parent << endl;
         throw TreeCorrelatorException(ss.str());
     }
-}
-
-vector<string> TreeCorrelator::split_names(std::string name) {
-    vector<string> names;
-    vector<string> name_tokens = TokenizeString(name, "_");
-
-    if (name_tokens.size() > 1) {
-        string base_name;
-        for (vector<string>::iterator it = name_tokens.begin();
-                it != name_tokens.end() - 1;
-                ++it)
-            base_name += (*it) + "_";
-
-        if (name_tokens.back().find("-") != string::npos ||
-                name_tokens.back().find(",") != string::npos) {
-            vector<string> comma_token =
-                    TokenizeString(name_tokens.back(), ",");
-            for (vector<string>::iterator itc = comma_token.begin();
-                    itc != comma_token.end();
-                    ++itc) {
-                vector<string> range_tokens = TokenizeString((*itc), "-");
-                int range_min = stoi(range_tokens[0]);
-                int range_max = range_min;
-                if (range_tokens.size() > 1)
-                    range_max = stoi(range_tokens[1]);
-
-                for (int i = range_min; i <= range_max; ++i) {
-                    stringstream ss;
-                    ss << i;
-                    names.push_back(base_name + ss.str());
-                }
-            }
-        } else {
-            names.push_back(base_name + name_tokens.back());
-        }
-    } else {
-        names.push_back(name);
-    }
-    return names;
 }
 
 void TreeCorrelator::createPlace(std::map<std::string, std::string>& params,
@@ -169,27 +107,52 @@ void TreeCorrelator::createPlace(std::map<std::string, std::string>& params,
 }
 
 void TreeCorrelator::buildTree() {
-    string cfg = Globals::get()->GetConfigFileName();
-    pugi::xml_document doc;
-
     Messenger m;
-    m.start("Creating TreeCorrelator");
-    pugi::xml_parse_result result = doc.load_file(cfg.c_str());
-    if (!result) {
-        stringstream ss;
-        ss << "DetectorDriver: error parsing file " << cfg;
-        ss << " : " << result.description();
-        m.fail();
-        throw IOException(ss.str());
-    }
+    m.start("Growing TreeCorrelator");
 
-    pugi::xml_node tree = doc.child("Configuration").child("TreeCorrelator");
-    bool verbose = tree.attribute("verbose").as_bool(false);
-
-    Walker walker;
-    walker.traverseTree(tree, string(tree.attribute("name").value()), verbose);
+    TreeCorrelatorXmlParser parser;
+    parser.ParseNode(this);
 
     m.done();
+}
+
+vector<string> TreeCorrelator::split_names(std::string name) {
+    vector<string> names;
+    vector<string> name_tokens = TokenizeString(name, "_");
+
+    if (name_tokens.size() > 1) {
+        string base_name;
+        for (vector<string>::iterator it = name_tokens.begin();
+             it != name_tokens.end() - 1;
+             ++it)
+            base_name += (*it) + "_";
+
+        if (name_tokens.back().find("-") != string::npos ||
+            name_tokens.back().find(",") != string::npos) {
+            vector<string> comma_token =
+                    TokenizeString(name_tokens.back(), ",");
+            for (vector<string>::iterator itc = comma_token.begin();
+                 itc != comma_token.end();
+                 ++itc) {
+                vector<string> range_tokens = TokenizeString((*itc), "-");
+                int range_min = stoi(range_tokens[0]);
+                int range_max = range_min;
+                if (range_tokens.size() > 1)
+                    range_max = stoi(range_tokens[1]);
+
+                for (int i = range_min; i <= range_max; ++i) {
+                    stringstream ss;
+                    ss << i;
+                    names.push_back(base_name + ss.str());
+                }
+            }
+        } else {
+            names.push_back(base_name + name_tokens.back());
+        }
+    } else {
+        names.push_back(name);
+    }
+    return names;
 }
 
 TreeCorrelator::~TreeCorrelator() {
