ee592b441df70ddcf51949bd9758c05aec57638a
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-02-23 11:12:56 -0500

    Updating RootFitter so that it fits properly. Updating unit tests.

--- Analysis/Resources/include/RootFitter.hpp
+++ Analysis/Resources/include/RootFitter.hpp
@@ -2,23 +2,33 @@
 /// @brief Class to handle fitting traces using ROOT
 /// @author S. V. Paulauskas
 /// @date December 18, 2016
-#ifndef PIXIESUITE_ROOTFITTER_HPP
-#define PIXIESUITE_ROOTFITTER_HPP
+#ifndef _PIXIESUITE_ROOTFITTER_HPP_
+#define _PIXIESUITE_ROOTFITTER_HPP_
+
+#include <TF1.h>
+#include <TFitResult.h>
+#include <TGraphErrors.h>
 
 #include "TimingDriver.hpp"
 
+class VandleTimingFunction;
+
 class RootFitter : public TimingDriver {
 public:
-    RootFitter() {};
+    RootFitter();
 
-    ~RootFitter() {};
+    ~RootFitter();
 
-    /// Perform CFD analysis on the waveform using the pol2 algorithm.
+    /// Perform fitting analysis using ROOT
     double CalculatePhase(const std::vector<double> &data,
                           const std::pair<double, double> &pars,
                           const std::pair<unsigned int, double> &maxInfo,
                           std::pair<double, double> baseline);
+
+private:
+    TF1 *func_;
+    VandleTimingFunction *vandleTimingFunction_;
 };
 
 
-#endif //PIXIESUITE_ROOTFITTER_HPP
+#endif //#ifndef _PIXIESUITE_ROOTFITTER_HPP_
