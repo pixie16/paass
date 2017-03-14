5a21793ff8489da5236bab309ffa35578b63e639
Rin <ryokoyam@utk.edu>
2017-03-14 10:26:54 -0400

    UtkUnpacker.cpp was modified to load eventWidth from Globals

--- Analysis/Utkscan/core/source/UtkUnpacker.cpp
+++ Analysis/Utkscan/core/source/UtkUnpacker.cpp
@@ -60,6 +60,9 @@ void UtkUnpacker::ProcessRawEvent(ScanInterface *addr_/*=NULL*/) {
         PrintProcessingTimeInformation(systemStartTime, times(&systemTimes),
             GetEventStartTime(), eventCounter);
 
+    //set eventWidth from Globals (loaded from config file)
+    eventWidth = Globals::get()->GetEventLengthInTicks();
+ 
     if (Globals::get()->HasRejectionRegion()) {
         double eventTime = (GetEventStartTime() - GetFirstTime()) *
                                 Globals::get()->GetClockInSeconds();
