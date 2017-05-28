82ca7f6ef918a8ff9678f60957ee155fe8c7c578
Unknown <tking.91@gmail.com>
2017-05-28 12:33:46 -0400

    enabling dual style TraceDelay node
    This commit allows the code to read either the old single trace delay style or the new map style; and added a catch for improper formated new style

--- Analysis/Utkscan/core/source/GlobalsXmlParser.cpp
+++ Analysis/Utkscan/core/source/GlobalsXmlParser.cpp
@@ -272,27 +272,40 @@ void GlobalsXmlParser::ParseTraceNode(const pugi::xml_node &node, Globals *globa
 
     if (!node.child("TraceDelay").empty()) {
         std::map<std::string, unsigned int> delays;
-        for (pugi::xml_node_iterator tdit = node.child("TraceDelay").begin();
-             tdit != node.child("TraceDelay").end(); ++tdit) {
-            std::string name = tdit->name();
-            unsigned int tdelay = tdit->attribute("value").as_uint();
-            delays.insert(make_pair(name, tdelay));
-        }
-        globals->SetTraceDelay(delays);
+        if (!node.child("TraceDelay").attribute("value").empty()) {
+            delays.insert(make_pair("global", node.child("TraceDelay").attribute("value").as_uint()));
+            globals->SetTraceDelay(delays);
 
-        sstream_ << "Trace Delays:";
-        messenger_.detail(sstream_.str());
-        for (std::map<std::string, unsigned int>::const_iterator tdloop =
-                delays.begin(); tdloop != delays.end(); tdloop++) {
+            sstream_ << "Trace Delay : " << globals->GetTraceDelayInNs();
+            messenger_.detail(sstream_.str());
             sstream_.str("");
-            sstream_ << tdloop->first << " = " << tdloop->second << " ns";
-            messenger_.detail(sstream_.str(), 1);
 
+        } else {
+            if (node.child("TraceDelay").child("global").empty()) {
+                throw invalid_argument(CriticalAttributeMessage("Global Trace Delay must be defined"));
+            } else {
+                for (pugi::xml_node_iterator tdit = node.child("TraceDelay").begin();
+                     tdit != node.child("TraceDelay").end(); ++tdit) {
+                    std::string name = tdit->name();
+                    unsigned int tdelay = tdit->attribute("value").as_uint();
+                    delays.insert(make_pair(name, tdelay));
+                }
+                globals->SetTraceDelay(delays);
+
+                sstream_ << "Trace Delays:";
+                messenger_.detail(sstream_.str());
+                for (std::map<std::string, unsigned int>::const_iterator tdloop =
+                        delays.begin(); tdloop != delays.end(); tdloop++) {
+                    sstream_.str("");
+                    sstream_ << tdloop->first << " = " << tdloop->second << " ns";
+                    messenger_.detail(sstream_.str(), 1);
+                }
+            }
         }
-    } else
-        throw invalid_argument(CriticalNodeMessage("TraceDelay"));
+    }else {
+        throw invalid_argument(CriticalNodeMessage("TraceDelay"));}
 
-    sstream_.str("");
+        sstream_.str("");
     if (!node.child("QdcCompression").empty())
         globals->SetQdcCompression(
                 node.child("QdcCompression").attribute("value").as_double());
