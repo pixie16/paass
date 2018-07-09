7a73294a5bb36443b2d82dbb0855b9f19522ac30
T. T. King <tking.91@gmail.com>
2018-07-09 17:48:58 +0900

    added type:subtype to the ignored list
    we can now use "type:subtype" as a valid marker to ignore waveform analysis.

--- Analysis/Utkscan/analyzers/source/WaveformAnalyzer.cpp
+++ Analysis/Utkscan/analyzers/source/WaveformAnalyzer.cpp
@@ -26,8 +26,8 @@ WaveformAnalyzer::WaveformAnalyzer(const std::set<std::string> &ignoredTypes)
 
 void WaveformAnalyzer::Analyze(Trace &trace, const ChannelConfiguration &cfg) {
     TraceAnalyzer::Analyze(trace, cfg);
+    if (trace.IsSaturated() || trace.empty() || ignoredTypes_.find(cfg.GetType()) != ignoredTypes_.end() || ignoredTypes_.find(cfg.GetType()+":"+cfg.GetSubtype()) != ignoredTypes_.end()) {
 
-    if (trace.IsSaturated() || trace.empty() || ignoredTypes_.find(cfg.GetType()) != ignoredTypes_.end()) {
         trace.SetHasValidAnalysis(false);
         EndAnalyze();
         return;
