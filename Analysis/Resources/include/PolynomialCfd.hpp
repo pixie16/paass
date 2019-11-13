8e75328c698f2e625d0562d6c64469108f343a6d
T. T. King <tking.91@gmail.com>
2019-11-13 11:28:00 -0500

    New Pol1 method for poly cfd
    We now have a 1st order poly nominal for the poly cfd; as well as the ability to switch between the 1st and 2nd order (note this is for the leading "fit") This choice is selectable from the XML and defaults to the linear
    
    Fixed a bug in the CfdAnalyzer, where were were passing CFD:D and CFD:L rather than CFD:F and CFD:D (although F is the only parameter we need for the poly cfd).

--- Analysis/Resources/include/PolynomialCfd.hpp
+++ Analysis/Resources/include/PolynomialCfd.hpp
@@ -5,11 +5,16 @@
 #ifndef PIXIESUITE_POLYNOMIALCFD_HPP
 #define PIXIESUITE_POLYNOMIALCFD_HPP
 
+#include "Exceptions.hpp"
 #include "TimingDriver.hpp"
 
 class PolynomialCfd : public TimingDriver {
 public:
-    PolynomialCfd() {};
+    /// Default constructor 
+    PolynomialCfd();
+
+    ///Constructor taking poly method 1 or 2 as input
+    PolynomialCfd(const int &method);
 
     ~PolynomialCfd() {};
 
@@ -18,6 +23,16 @@ public:
                           const std::pair<double, double> &pars,
                           const std::pair<unsigned int, double> &max,
                           const std::pair<double, double> baseline);
+
+private:
+ /// Calculates the phase using a 2nd order polynomial around the threshold
+ double CalcPoly2Phase(const double &thresh, const double &traceMax_, const std::vector<double> &data);
+
+ /// Calculates the phase using a 1st order polynomial (linear) around the threshold
+ double CalcPoly1Phase(const double &thresh, const double &traceMax_, const std::vector<double> &data);
+
+ /// order of the polynomial to use around the threshold
+ int polyMethod_;
 };
 
 #endif //PIXIESUITE_POLYNOMIALCFD_HPP
--- Analysis/Resources/source/PolynomialCfd.cpp
+++ Analysis/Resources/source/PolynomialCfd.cpp
@@ -10,24 +10,67 @@
 
 using namespace std;
 
+PolynomialCfd::PolynomialCfd(){
+    polyMethod_ = 1;
+}
+
+PolynomialCfd::PolynomialCfd(const int &method){
+    
+    switch ( method ) {
+        case 1:
+        case 2:
+            polyMethod_ = method;
+            break;
+        default:
+            throw GeneralException("PolynomialCfd::Unsupported Interpolation method!");       
+    }
+    
+}
+
+
 /// Perform CFD analysis on the waveform.
-double PolynomialCfd::CalculatePhase(const std::vector<double> &data, const std::pair<double, double> &pars,
-                                     const std::pair<unsigned int, double> &max,
-                                     const std::pair<double, double> baseline) {
+double PolynomialCfd::CalculatePhase(const std::vector<double> &data, const std::pair<double, double> &pars,const std::pair<unsigned int, double> &max, const std::pair<double, double> baseline) {
     if (data.size() == 0)
         throw range_error("PolynomialCfd::CalculatePhase - The data vector was empty!");
     if (data.size() < max.first)
-        throw range_error("PolynomialCfd::CalculatePhase - The maximum "
-                                  "position is larger than the size of the "
-                                  "data vector.");
+        throw range_error("PolynomialCfd::CalculatePhase - The maximum position is larger than the size of the data vector.");
 
     double threshold = pars.first * max.second;
     double phase = -9999;
+  
+    if (polyMethod_ == 1) {
+        phase = CalcPoly1Phase(threshold,max.first,data);
+        //  cout<< "PolyCFD_method=: "<<polyMethod_<<"   Returned "<< phase << " as the phase      threshold(%) = "<< threshold << " (" << pars.first << ")" <<endl;
+    } else if (polyMethod_ == 2){
+        phase = CalcPoly2Phase(threshold,max.first,data);
+    }
+    
+    return phase;
+}
+
+double PolynomialCfd::CalcPoly1Phase(const double &thresh, const double &traceMax_, const std::vector<double> &data) {
+    double pol1Phase_ = -9999;
+
+    vector<double> result;
+    for (unsigned int cfdIndex = traceMax_; cfdIndex > 0; cfdIndex--) {
+        if (data[cfdIndex - 1] < thresh && data[cfdIndex] >= thresh) {
+            // Fit the rise of the trace to a 2nd order polynomial.
+            result = Polynomial::CalculatePoly1(data, cfdIndex - 1).second;
+
+            pol1Phase_ = (thresh - result[0]) / result[1];
+            break;
+        }
+    }
+    return pol1Phase_;
+}
+
+double PolynomialCfd::CalcPoly2Phase(const double &thresh, const double &traceMax_, const std::vector<double> &data){
+    double pol2Phase_=-9999;
     float multiplier = 1.;
 
     vector<double> result;
-    for (unsigned int cfdIndex = max.first; cfdIndex > 0; cfdIndex--) {
-        if (data[cfdIndex - 1] < threshold && data[cfdIndex] >= threshold) {
+    for (unsigned int cfdIndex = traceMax_; cfdIndex > 0; cfdIndex--) {
+        if (data[cfdIndex - 1] < thresh && data[cfdIndex] >= thresh) {
             // Fit the rise of the trace to a 2nd order polynomial.
             result = Polynomial::CalculatePoly2(data, cfdIndex - 1).second;
 
@@ -35,11 +78,10 @@ double PolynomialCfd::CalculatePhase(const std::vector<double> &data, const std:
             if (result[2] > 1)
                 multiplier = -1.;
 
-            phase = (-result[1] + multiplier * sqrt(result[1] * result[1] - 4 * result[2] * (result[0] - threshold)))
-                    / (2 * result[2]);
+            pol2Phase_ = (-result[1] + multiplier * sqrt(result[1] * result[1] - 4 * result[2] * (result[0] - thresh))) / (2 * result[2]);
 
             break;
         }
     }
-    return phase;
+    return pol2Phase_;
 }
