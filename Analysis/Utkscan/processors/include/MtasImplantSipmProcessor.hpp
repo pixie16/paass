d207fd32b2ecbb317e05dec6c464957a23b8eb91
truland <tnfirebird1979@gmail.com>
2023-11-22 14:50:43 -0500

    Made CalculatePosition function const as well as the vector passed in by ref as const

--- Analysis/Utkscan/processors/include/MtasImplantSipmProcessor.hpp
+++ Analysis/Utkscan/processors/include/MtasImplantSipmProcessor.hpp
@@ -15,50 +15,50 @@
 #include "RawEvent.hpp"
 
 class MtasImplantSipmProcessor : public EventProcessor {
-   public:
-    /**Constructor */
-    MtasImplantSipmProcessor();
+	public:
+		/**Constructor */
+		MtasImplantSipmProcessor();
 
-       /**Constructor */
-    MtasImplantSipmProcessor(double yso_scale_, double yso_offset_, double yso_thresh_, double oqdc_yso_thresh_, double dyh_thresh_, double dyh_qdc_thresh_, double dyl_thresh_, double dyl_qdc_thresh_, double dyh_upperThresh_);
+		/**Constructor */
+		MtasImplantSipmProcessor(double yso_scale_, double yso_offset_, double yso_thresh_, double oqdc_yso_thresh_, double dyh_thresh_, double dyh_qdc_thresh_, double dyl_thresh_, double dyl_qdc_thresh_, double dyh_upperThresh_);
 
-    /** Deconstructor */
-    ~MtasImplantSipmProcessor() = default;
+		/** Deconstructor */
+		~MtasImplantSipmProcessor() = default;
 
-    /** Preprocess the event
+		/** Preprocess the event
 		 * \param [in] event : the event to preprocess
 		 * \return true if successful
 		 */
-    bool PreProcess(RawEvent& event);
+		bool PreProcess(RawEvent& event);
 
-    /** Process the event
+		/** Process the event
 		 * \param [in] event : the event to process
 		 * \return true if successful
 		 */
-    bool Process(RawEvent& event);
+		bool Process(RawEvent& event);
 
-    /** Declares the plots for the class */
-    void DeclarePlots(void);
+		/** Declares the plots for the class */
+		void DeclarePlots(void);
 
-    double CalOnboardQDC(int bkg, int waveform, const std::vector<unsigned> oqdcs, double bkgTimeScaling = 1, double waveformTimeScaling = 1);
+		double CalOnboardQDC(int bkg, int waveform, const std::vector<unsigned> oqdcs, double bkgTimeScaling = 1, double waveformTimeScaling = 1);
 
-    std::pair<int, int> ComputeSiPmPixelLoc(int xmlLocation_);
+		std::pair<int, int> ComputeSiPmPixelLoc(int xmlLocation_);
 
-    void FillRootStruct(ChanEvent* evt, double& onboardqdc, const std::pair<int,int> &positions = {0,0});
+		void FillRootStruct(ChanEvent* evt, double& onboardqdc, const std::pair<int,int> &positions = {0,0});
 
-    std::pair<double,double> CalculatePosition(std::vector<std::vector<double>>& data);
+		std::pair<double,double> CalculatePosition(const std::vector<std::vector<double>>& data) const;
 
-   private:
-    std::string PixieRevision;  //! pixie revision
-    processor_struct::MTASIMPLANT mtasImplStruct;
-    double EandQDC_down_scaling_;
-    std::pair<int, int> dammSiPm_pixelShifts;
-    double yso_thresh,yso_scale,yso_offset,oqdc_yso_thresh;
-    double dyh_thresh;
-    double dyh_qdc_thresh;
-    double dyl_thresh;
-    double dyl_qdc_thresh;
-    double dyh_upperThresh;
+	private:
+		std::string PixieRevision;  //! pixie revision
+		processor_struct::MTASIMPLANT mtasImplStruct;
+		double EandQDC_down_scaling_;
+		std::pair<int, int> dammSiPm_pixelShifts;
+		double yso_thresh,yso_scale,yso_offset,oqdc_yso_thresh;
+		double dyh_thresh;
+		double dyh_qdc_thresh;
+		double dyl_thresh;
+		double dyl_qdc_thresh;
+		double dyh_upperThresh;
 };
 
 #endif
