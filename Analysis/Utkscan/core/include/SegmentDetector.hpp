6fbc1bef91b441a3667da277ad2162efd97c5f67
truland <tnfirebird1979@gmail.com>
2023-11-22 14:26:33 -0500

    Fixed noexcept issues raised by jmattspartacus in PR#315, as well as fixing issue about hard-coded QDC values noticed by me

--- Analysis/Utkscan/core/include/SegmentDetector.hpp
+++ Analysis/Utkscan/core/include/SegmentDetector.hpp
@@ -10,110 +10,126 @@
 #include "Globals.hpp"
 
 class SegmentDetector {
-   public:
-    /** Constructor */
-    SegmentDetector() {
-        segFront_ = nullptr;
-        segBack_ = nullptr;
-    };
-    /** Destructor */
-    ~SegmentDetector() = default;
+	public:
+		/** Constructor */
+		SegmentDetector() {
+			segFront_ = nullptr;
+			segBack_ = nullptr;
+		};
+		/** Destructor */
+		~SegmentDetector() = default;
 
-    virtual ChanEvent* GetSegFront() {
-        return segFront_;
-    };
-    virtual ChanEvent* GetSegBack() {
-        return segBack_;
-    };
+		virtual ChanEvent* GetSegFront() {
+			return segFront_;
+		};
+		virtual ChanEvent* GetSegBack() {
+			return segBack_;
+		};
 
-    virtual void SetSegFront(ChanEvent* a) {
-        segFront_ = a;
-    };
-    virtual void SetSegBack(ChanEvent* a) {
-        segBack_ = a;
-    };
+		virtual void SetSegFront(ChanEvent* a) {
+			segFront_ = a;
+		};
+		virtual void SetSegBack(ChanEvent* a) {
+			segBack_ = a;
+		};
 
-    virtual void SetPixieRev(string &a) {
-        PixieRev = a;
-    };
+		virtual void SetPixieRev(string &a) {
+			PixieRev = a;
+		};
 
-    virtual string GetPixieRev(){
-        return PixieRev;
-    };
+		virtual string GetPixieRev() noexcept{
+			return PixieRev;
+		};
 
-    virtual bool IsValidSegment() const { 
-        return segBack_ != nullptr and segFront_ != nullptr;
-    };
+		virtual bool IsValidSegment() const  noexcept{ 
+			return segBack_ != nullptr and segFront_ != nullptr;
+		};
 
-    virtual double GetSegAvgEnergy() const {
-        if (IsValidSegment()) {
-            return (segFront_->GetCalibratedEnergy() + segBack_->GetCalibratedEnergy()) / 2.0;
-        } else {
-            return 0.0;
-        }
-    }
-
-    virtual double GetSegQDC() const {
-        if (IsValidSegment()) {
-            double fQDC = segFront_->GetQdc().at(2) - segFront_->GetQdc().at(0);
-            double bQDC = segBack_->GetQdc().at(2) - segBack_->GetQdc().at(0);
-			return ((fQDC + bQDC) /2.0);
-        } else {
-			return 0.0;
+		virtual double GetSegAvgEnergy() const  noexcept{
+			if (IsValidSegment()) {
+				return (segFront_->GetCalibratedEnergy() + segBack_->GetCalibratedEnergy()) / 2.0;
+			} else {
+				return 0.0;
+			}
 		}
-    }
 
-    virtual double GetSegTimeAvgInNs() const {
-        if (not IsValidSegment()) {
-            throw "Unable to get SegmentDetector time average. Not valid SegmentDetector";
-        }
-        double clockInSeconds;
-        if (PixieRev == "F") {
-            clockInSeconds = Globals::get()->GetClockInSeconds(segFront_->GetChanID().GetModFreq());
-        } else {
-            clockInSeconds = Globals::get()->GetClockInSeconds();
-        }
+		virtual double GetSegQDC(unsigned int erg,unsigned int bkg) const{
+			if (IsValidSegment()) {
+				double fQDC = segFront_->GetQdc().at(erg) - segFront_->GetQdc().at(bkg);
+				double bQDC = segBack_->GetQdc().at(erg) - segBack_->GetQdc().at(bkg);
+				return ((fQDC + bQDC) /2.0);
+			} else {
+				return 0.0;
+			}
 
-        return ( ((segFront_->GetTimeSansCfd() + segBack_->GetTimeSansCfd()) / 2.0)  * clockInSeconds * 1.0e9);
-    }
+		}
 
-    virtual double GetSegPosition() const {
-        return (segFront_->GetCalibratedEnergy() - segBack_->GetCalibratedEnergy()) / (segFront_->GetCalibratedEnergy() + segBack_->GetCalibratedEnergy());
-    }
+		virtual double GetSegQDC() const {
+			if (IsValidSegment()) {
+				double fQDC = segFront_->GetQdc().at(2) - segFront_->GetQdc().at(0);
+				double bQDC = segBack_->GetQdc().at(2) - segBack_->GetQdc().at(0);
+				return ((fQDC + bQDC) /2.0);
+			} else {
+				return 0.0;
+			}
+		}
 
-    virtual double GetSegTdiffInNs() const {
-        if (not IsValidSegment()) {
-            throw "Unable to get Tdiff. Not valid SegmentDetector";
-        }
-        double clockInSeconds;
-        if (PixieRev == "F") {
-            clockInSeconds = Globals::get()->GetClockInSeconds(segFront_->GetChanID().GetModFreq());
-        } else {
-            clockInSeconds = Globals::get()->GetClockInSeconds();
-        }
+		virtual double GetSegTimeAvgInNs() const {
+			if (not IsValidSegment()) {
+				throw "Unable to get SegmentDetector time average. Not valid SegmentDetector";
+			}
+			double clockInSeconds;
+			if (PixieRev == "F") {
+				clockInSeconds = Globals::get()->GetClockInSeconds(segFront_->GetChanID().GetModFreq());
+			} else {
+				clockInSeconds = Globals::get()->GetClockInSeconds();
+			}
 
-        return (segFront_->GetTimeSansCfd() - segBack_->GetTimeSansCfd()) * clockInSeconds * 1.0e9;
-    }
+			return ( ((segFront_->GetTimeSansCfd() + segBack_->GetTimeSansCfd()) / 2.0)  * clockInSeconds * 1.0e9);
+		}
 
-    virtual double GetFrontEnergy() const {
-        if (segFront_ == nullptr) {
-            return 0.0;
-        } else {
-            return segFront_->GetCalibratedEnergy();
-        }
-    }
+		virtual double GetSegPosition() const {
+			if( IsValidSegment() ){
+				return (segFront_->GetCalibratedEnergy() - segBack_->GetCalibratedEnergy()) / (segFront_->GetCalibratedEnergy() + segBack_->GetCalibratedEnergy());
+			}else{
+				return -1.0e12;
+			}
+		}
 
-    virtual double GetBackEnergy() const {
-        if (segBack_ == nullptr) {
-            return 0.0;
-        } else {
-            return segBack_->GetCalibratedEnergy();
-        }
-    }
+		virtual double GetSegTdiffInNs() const {
+			if( IsValidSegment() ){
+				double clockInSeconds;
+				if (PixieRev == "F") {
+					clockInSeconds = Globals::get()->GetClockInSeconds(segFront_->GetChanID().GetModFreq());
+				} else {
+					clockInSeconds = Globals::get()->GetClockInSeconds();
+				}
 
-    protected:
-    ChanEvent* segFront_;
-    ChanEvent* segBack_;
-    string PixieRev;
+				return (segFront_->GetTimeSansCfd() - segBack_->GetTimeSansCfd()) * clockInSeconds * 1.0e9;
+			}else{
+				return -1.0e12;
+			}
+		}
+
+		virtual double GetFrontEnergy() const {
+			if (segFront_ == nullptr) {
+				return 0.0;
+			} else {
+				return segFront_->GetCalibratedEnergy();
+			}
+		}
+
+		virtual double GetBackEnergy() const {
+			if (segBack_ == nullptr) {
+				return 0.0;
+			} else {
+				return segBack_->GetCalibratedEnergy();
+			}
+		}
+
+	protected:
+		ChanEvent* segFront_;
+		ChanEvent* segBack_;
+		string PixieRev;
 };
 #endif
