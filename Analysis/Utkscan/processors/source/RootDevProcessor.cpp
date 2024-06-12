7e9c2496c093da8c048979deeb7bdb4d2fc4ec75
T. T. King <tking.91@gmail.com>
2024-06-12 12:59:06 +0900

    RootDevProcessor:: Add ability to have arbitrary types added to root dev output. These are Extra to the RD type, as well as in addition to their original processors if those are enabled.

--- Analysis/Utkscan/processors/source/RootDevProcessor.cpp
+++ Analysis/Utkscan/processors/source/RootDevProcessor.cpp
@@ -8,11 +8,23 @@
 #include "DetectorDriver.hpp"
 #include "RawEvent.hpp"
 #include "RootDevProcessor.hpp"
+#include "StringManipulationFunctions.hpp"
 
 using namespace std;
 
-RootDevProcessor::RootDevProcessor() : EventProcessor() {
-    associatedTypes.insert("RD");
+RootDevProcessor::RootDevProcessor(std::string additionaltypes) : EventProcessor() {
+    typesToRead = std::vector<std::string>();
+    typesToRead.push_back("RD");
+    //parse additionaltypes here
+    if( not additionaltypes.empty()){
+        auto typelist = StringManipulation::TokenizeString(additionaltypes,",");
+        for( size_t ii = 0; ii < typelist.size(); ++ii ){
+            typesToRead.push_back(typelist.at(ii));
+        }
+    }
+    for( size_t ii = 0; ii < typesToRead.size(); ++ii ){
+        associatedTypes.insert(typesToRead.at(ii));
+    }
 }
 
 bool RootDevProcessor::PreProcess(RawEvent &event) {
@@ -26,49 +38,52 @@ bool RootDevProcessor::Process(RawEvent &event) {
     if (!EventProcessor::Process(event))
         return false;
 
-    static const auto &Events = event.GetSummary("RD", true)->GetList();
+    for(size_t ii = 0; ii < typesToRead.size(); ++ii){
+        const auto &Events = event.GetSummary(typesToRead.at(ii),true)->GetList();
 
-    for (auto it = Events.begin(); it != Events.end(); it++) {
-        RDstruct.energy = (*it)->GetCalibratedEnergy();
-        RDstruct.rawEnergy = (*it)->GetEnergy();
-        RDstruct.timeSansCfd = (*it)->GetTimeSansCfd() ;
-        RDstruct.time = (*it)->GetTime() ;
-        RDstruct.timeSansCfdInNs = (*it)->GetTimeSansCfdInNs() ;
-        RDstruct.timeInNs = (*it)->GetTimeInNs() ;
-        RDstruct.cfdForcedBit = (*it)->GetCfdForcedTriggerBit();
-        RDstruct.cfdFraction = (*it)->GetCfdFractionalTime();
-        RDstruct.cfdSourceBit = (*it)->GetCfdTriggerSourceBit();
-        RDstruct.detNum = (*it)->GetChanID().GetLocation();
-        RDstruct.channelID= (*it)->GetID();
-        RDstruct.crateNum = (*it)->GetCrateNumber();
-        RDstruct.modNum = (*it)->GetModuleNumber();
-        RDstruct.chanNum = (*it)->GetChannelNumber();
-        RDstruct.subtype = (*it)->GetChanID().GetSubtype();
-        RDstruct.group = (*it)->GetChanID().GetGroup();
-        RDstruct.pileup = (*it)->IsPileup();
-        RDstruct.saturation = (*it)->IsSaturated();
-        RDstruct.revision = (*it)->GetChanID().GetRevision();
-        RDstruct.tickToNS = (*it)->GetChanID().GetTickToNS();
-        RDstruct.adcTickToNS = (*it)->GetChanID().GetAdcTickToNS();
 
-        if ((*it)->GetTrace().size() > 0) {
-            RDstruct.hasValidTimingAnalysis = (*it)->GetTrace().HasValidTimingAnalysis();
-            RDstruct.hasValidWaveformAnalysis = (*it)->GetTrace().HasValidWaveformAnalysis();
-            RDstruct.baseline = (*it)->GetTrace().GetBaselineInfo().first;
-            RDstruct.stdBaseline = (*it)->GetTrace().GetBaselineInfo().second;
-            RDstruct.trace = (*it)->GetTrace();
-            RDstruct.maxPos = (*it)->GetTrace().GetMaxInfo().first;
-            RDstruct.maxVal = (*it)->GetTrace().GetMaxInfo().second;
-            RDstruct.extMaxVal = (*it)->GetTrace().GetExtrapolatedMaxInfo().second;
-            RDstruct.tqdc = (*it)->GetTrace().GetQdc();
-            RDstruct.highResTime = (*it)->GetHighResTimeInNs();
-            RDstruct.phase = (*it)->GetTrace().GetPhase() * (*it)->GetChanID().GetAdcTickToNS();
+        for (auto it = Events.begin(); it != Events.end(); it++) {
+            RDstruct.energy = (*it)->GetCalibratedEnergy();
+            RDstruct.rawEnergy = (*it)->GetEnergy();
+            RDstruct.timeSansCfd = (*it)->GetTimeSansCfd() ;
+            RDstruct.time = (*it)->GetTime() ;
+            RDstruct.timeSansCfdInNs = (*it)->GetTimeSansCfdInNs() ;
+            RDstruct.timeInNs = (*it)->GetTimeInNs() ;
+            RDstruct.cfdForcedBit = (*it)->GetCfdForcedTriggerBit();
+            RDstruct.cfdFraction = (*it)->GetCfdFractionalTime();
+            RDstruct.cfdSourceBit = (*it)->GetCfdTriggerSourceBit();
+            RDstruct.detNum = (*it)->GetChanID().GetLocation();
+            RDstruct.channelID= (*it)->GetID();
+            RDstruct.crateNum = (*it)->GetCrateNumber();
+            RDstruct.modNum = (*it)->GetModuleNumber();
+            RDstruct.chanNum = (*it)->GetChannelNumber();
+            RDstruct.subtype = (*it)->GetChanID().GetSubtype();
+            RDstruct.group = (*it)->GetChanID().GetGroup();
+            RDstruct.pileup = (*it)->IsPileup();
+            RDstruct.saturation = (*it)->IsSaturated();
+            RDstruct.revision = (*it)->GetChanID().GetRevision();
+            RDstruct.tickToNS = (*it)->GetChanID().GetTickToNS();
+            RDstruct.adcTickToNS = (*it)->GetChanID().GetAdcTickToNS();
+
+            if ((*it)->GetTrace().size() > 0) {
+                RDstruct.hasValidTimingAnalysis = (*it)->GetTrace().HasValidTimingAnalysis();
+                RDstruct.hasValidWaveformAnalysis = (*it)->GetTrace().HasValidWaveformAnalysis();
+                RDstruct.baseline = (*it)->GetTrace().GetBaselineInfo().first;
+                RDstruct.stdBaseline = (*it)->GetTrace().GetBaselineInfo().second;
+                RDstruct.trace = (*it)->GetTrace();
+                RDstruct.maxPos = (*it)->GetTrace().GetMaxInfo().first;
+                RDstruct.maxVal = (*it)->GetTrace().GetMaxInfo().second;
+                RDstruct.extMaxVal = (*it)->GetTrace().GetExtrapolatedMaxInfo().second;
+                RDstruct.tqdc = (*it)->GetTrace().GetQdc();
+                RDstruct.highResTime = (*it)->GetHighResTimeInNs();
+                RDstruct.phase = (*it)->GetTrace().GetPhase() * (*it)->GetChanID().GetAdcTickToNS();
+            }
+            if (!(*it)->GetQdc().empty()) {
+                RDstruct.qdcSums = (*it)->GetQdc();
+            }
+            pixie_tree_event_->rootdev_vec_.emplace_back(RDstruct);
+            RDstruct = processor_struct::ROOTDEV_DEFAULT_STRUCT;
         }
-        if (!(*it)->GetQdc().empty()) {
-            RDstruct.qdcSums = (*it)->GetQdc();
-        }
-        pixie_tree_event_->rootdev_vec_.emplace_back(RDstruct);
-        RDstruct = processor_struct::ROOTDEV_DEFAULT_STRUCT;
     }
 
     EndProcess();
